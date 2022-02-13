bcf0ac4f399407371ec914a9b647c638a78a698a
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-02-13 23:12:34 +0530

    Time: 35 ms (36.47%), Space: 15.5 MB (98.24%) - LeetHub

---
+++ 437-path-sum-iii/437-path-sum-iii.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int dfs(TreeNode* root, int targetSum){
+        if(root == NULL) return 0;
+        
+        targetSum -= root->val; // we subtract at each and evrey moments 
+        
+        int count = 0;
+        if(targetSum == 0) // at any point if target is zero then incresse the count += 1 ;
+            count++;
+        
+        return count + dfs(root->left, targetSum) + dfs(root->right, targetSum);
+    }
+    int pathSum(TreeNode* root, int targetSum) {
+        // we are checking for each and every head
+        // base case;
+        if(root == NULL) return 0;
+        return dfs(root,  targetSum) +  pathSum(root->left, targetSum) + pathSum(root->right , targetSum) ; 
+        // check for head // dfs(root , k);
+        //check for its right and left = // pathSum(root->left/right , target);
+    }
+};
