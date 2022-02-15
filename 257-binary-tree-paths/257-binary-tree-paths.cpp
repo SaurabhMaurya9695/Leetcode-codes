98ff9e4f72f34b52db64171d22b516e662f66529
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-02-15 22:40:26 +0530

    Time: 3 ms (83.57%), Space: 13.2 MB (48.00%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.cpp
+++ 257-binary-tree-paths/257-binary-tree-paths.cpp
@@ -1,35 +1,25 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-        void dfs(TreeNode* root,vector<string>&res,string ans)
-        {
-            if(root==NULL) return;
-            if(ans=="") ans=to_string(root->val);
-            else ans+="->"+to_string(root->val);
-            if(root->left==NULL and root->right==NULL){
-                res.push_back(ans);
-                return;
-            }
-            if(root->left!=NULL){
-                dfs(root->left,res,ans);
-            }
-            if(root->right!=NULL){
-                dfs(root->right,res,ans);
-            }
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> ans;
+        tree(root,"", ans);
+        return ans;
     }
-        vector<string> binaryTreePaths(TreeNode* root) {
-            vector<string> res;
-            dfs(root,res,"");
-            return res;
+    void tree(TreeNode* root, string s, vector<string>& ans) // s is unique to each recursive call and changes when function goes from parent->child or child->parent
+    {
+        if(root==NULL)
+            return;
+        if(root->right!=NULL || root->left!=NULL ) //If a node is not a leaf, just add the value to the string and traverse it's child nodes
+        {
+            s+=to_string(root->val)+"->";
+            tree(root->left, s, ans);
+            tree(root->right, s, ans);
         }
+        else //if a node is a leaf node add its value to the string and now push this string to the vector of Strings
+        {
+            s+=to_string(root->val);
+            ans.push_back(s);
+            return;
+        }
+    }
 };
