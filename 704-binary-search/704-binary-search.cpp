7e3e8c9e5c0472b454f6131679b76355157060c1
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-02-16 13:26:58 +0530

    Time: 23 ms (99.45%), Space: 27.6 MB (63.92%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,13 +1,41 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        if( binary_search(nums.begin() , nums.end() , target))
+        // if( binary_search(nums.begin() , nums.end() , target))
+        // {
+        //     return lower_bound(nums.begin() , nums.end() , target) - nums.begin();
+        // }
+        // else{
+        //     return -1;
+        // }
+        
+        
+        int start = 0;
+        int end = nums.size() - 1;
+        while(start <= end)
         {
-            return lower_bound(nums.begin() , nums.end() , target) - nums.begin();
+            int mid = start + (end - start) / 2;
+            if(nums[mid] == target)
+                return mid ;
+            else if(nums[mid] > target)
+            {
+                end = mid - 1 ;
+            }
+            else{
+                start =  mid + 1 ;
+            }
         }
-        else{
-            return -1;
-        }
+        return -1;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 };
 static int x=[](){
