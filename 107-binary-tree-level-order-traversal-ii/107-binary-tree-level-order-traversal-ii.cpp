47d4c38a2d538a8ce587abfb156b0d62cd09cf82
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-02-20 12:11:29 +0530

    Time: 11 ms (23.87%), Space: 13.7 MB (5.35%) - LeetHub

--- 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.cpp
+++ 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.cpp
@@ -11,31 +11,51 @@
  */
 class Solution {
 public:
-    vector<vector<int>> levelOrderBottom(TreeNode* root) {
-        vector<vector<int>> res;
-        if(!root) return res;
-        queue<TreeNode*> q; 
-        q.push(root); 
-        
-        while(q.size() > 0 )
-        {  
-           int len = q.size();
-            vector<int> res2;
-            while(len--)
-            {
-               
-               TreeNode* curr = q.front(); 
-                q.pop();
-               res2.push_back(curr->val);
-               
-               if(curr->left) q.push(curr->left);
-               if(curr->right) q.push(curr->right);
-           } 
-            res.push_back(res2);
+    vector<vector<int>> res;
+    void dfs(TreeNode* root , int depth)
+    {
+        if(root == NULL) return;
+        if(res.size() == depth) // res ka size depth ke bababar ho;
+        {
+            res.push_back(vector<int>());
         }
-        reverse(res.begin() , res.end());
+        res[depth].push_back(root->val) ;
+        dfs(root->left , depth + 1);
+        dfs(root->right , depth + 1) ;
+            
+    }
+//     vector<vector<int>> levelOrderBottom(TreeNode* root) {
+// //         vector<vector<int>> res;
+// //         if(!root) return res;
+// //         queue<TreeNode*> q; 
+// //         q.push(root); 
+        
+// //         while(q.size() > 0 )
+// //         {  
+// //            int len = q.size();
+// //             vector<int> res2;
+// //             while(len--)
+// //             {
+               
+// //                TreeNode* curr = q.front(); 
+// //                 q.pop();
+// //                res2.push_back(curr->val);
+               
+// //                if(curr->left) q.push(curr->left);
+// //                if(curr->right) q.push(curr->right);
+// //            } 
+// //             res.push_back(res2);
+// //         }
+// //         reverse(res.begin() , res.end());
 
         
+// //         return res;
+// //     }
+// };
+    vector<vector<int>> levelOrderBottom(TreeNode* root)
+    {
+        dfs(root , 0);
+        reverse(res.begin() , res.end());
         return res;
     }
 };
