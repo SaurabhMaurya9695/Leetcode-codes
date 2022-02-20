a0279f48e7d991dce2646c8173bcecdd23d3ce94
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-02-20 12:27:32 +0530

    Time: 34 ms (46.18%), Space: 12 MB (9.94%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
@@ -18,32 +18,75 @@ public:
 };
 */
 
+// class Solution {
+// public:
+//     //vector<vector<int>> levelOrder(Node* root) {
+// //          vector<vector<int>> res;
+// //         if(!root) return res;
+// //         queue<Node*> q; 
+// //         q.push(root); 
+        
+// //         while(q.size() > 0 )
+// //         {  
+// //            int len = q.size();
+// //             vector<int> res2;
+// //             while(len--)
+// //             {
+               
+// //                Node* curr = q.front(); 
+// //                 q.pop();
+// //                res2.push_back(curr->val);
+               
+// //                for(Node* child : curr->children)
+// //                {
+// //                    q.push(child);
+// //                }
+// //            } 
+// //             res.push_back(res2);
+// //         }
+// //         return res ;
+//     }
+// };
+
 class Solution {
 public:
-    vector<vector<int>> levelOrder(Node* root) {
-         vector<vector<int>> res;
-        if(!root) return res;
-        queue<Node*> q; 
-        q.push(root); 
-        
-        while(q.size() > 0 )
-        {  
-           int len = q.size();
-            vector<int> res2;
-            while(len--)
-            {
-               
-               Node* curr = q.front(); 
-                q.pop();
-               res2.push_back(curr->val);
-               
-               for(Node* child : curr->children)
-               {
-                   q.push(child);
-               }
-           } 
-            res.push_back(res2);
-        }
+    
+    vector<vector<int>> res;
+    void dfs(Node* root , int depth)
+    {
+        if(root == NULL) return;
+        if(res.size() == depth)
+            res.push_back(vector<int>());
+        res[depth].push_back(root->val );
+        for(auto it : root->children)
+            dfs(it , depth + 1);
+         
+    }
+    vector<vector<int>> levelOrder(Node* root) 
+    {
+        dfs(root ,  0);
         return res ;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
