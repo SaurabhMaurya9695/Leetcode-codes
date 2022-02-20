5e141a45cf653537848c9ba1038a815c0104f64f
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-02-20 12:06:30 +0530

    Time: 0 ms (100.00%), Space: 13.8 MB (5.98%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.cpp
@@ -1,29 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        
-        vector<vector<int>> res;
-        if(!root) return res;
-        queue<TreeNode*> q; 
-        q.push(root); 
-        
-        while(q.size() > 0 )
-        {  
-           int len = q.size();
-            vector<int> res2;
-            while(len--)
-            {
-               
-               TreeNode* curr = q.front(); 
-                q.pop();
-               res2.push_back(curr->val);
-               
-               if(curr->left) q.push(curr->left);
-               if(curr->right) q.push(curr->right);
-           } 
-            res.push_back(res2);
+    vector<vector<int>> res;
+    void dfs(TreeNode* root , int depth)
+    {
+        if(root == NULL)
+        {
+            return;
         }
-        
+        if (res.size() == depth) {
+            res.push_back(vector<int>());
+        }
+        res[depth].push_back(root->val);
+        dfs(root->left , depth + 1);
+        dfs(root->right , depth + 1);
+    }
+    
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        dfs(root , 0);
         return res;
     }
 };
