33dbf2c8b60d467f2efd35f7f7e7236db8c9c41e
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-02-28 21:44:09 +0530

    Time: 13 ms (26.21%), Space: 7.5 MB (22.59%) - LeetHub

---
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    
+     bool isPossible(vector<int> pages , int books, int maxLoad, int totalStud){
+        int currStud = 1, currPages = 0;
+
+        for(int i=0; currStud <= totalStud && i<books; i++){
+
+            if(currPages + pages[i] <= maxLoad){
+                currPages += pages[i];
+            } else {
+                currStud++;
+                currPages = pages[i];
+            }
+
+        }
+
+        if(currStud > totalStud) return false;
+        return true;
+    }
+    
+    int findPages(vector<int> pages, int books , int students)
+    {
+        int start = *max_element(pages.begin() , pages.end()) ;// min no of pages can read by student
+        int sum = 0 ;
+        int n = books ;
+        for(int i =0 ; i< n; i++)
+        {
+            sum += pages[i];
+        }
+        int high = sum ;
+        int ans = -1;
+        while(start <= high)
+        {
+            int mid = start + ( high -  start) / 2;
+            if(isPossible(pages, books, mid, students) == true) // is should be a possible ans;
+            {
+                ans = mid;
+                high = mid - 1;
+            }
+            else{
+                start = mid + 1 ;
+            }
+        }
+        return ans;
+    }
+    int splitArray(vector<int>& nums, int m) {
+        int n = nums.size() ;
+        int x = findPages(nums , n ,  m) ;
+        return x;
+    }
+};
