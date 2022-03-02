7d65a5cac447f62f1900464cc7713c86bbf6f534
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-03-02 22:23:20 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (52.45%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.cpp
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -1,12 +1,27 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-      stringstream ss(s);
-       vector<string> v;
-        string word;
-        while (ss >> word) {
-          v.push_back(word);
-        } 
-        return v[v.size() - 1].size() ;
+      // stringstream ss(s);
+      //  vector<string> v;
+      //   string word;
+      //   while (ss >> word) {
+      //     v.push_back(word);
+      //   } 
+      //   return v[v.size() - 1].size() ;
+        
+        
+       int count=0;
+        bool flag=false;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]==' ' and flag==true){
+                return count;
+            }
+            if(s[i]!=' '){
+            flag=true;
+            count++;}
+            
+        }
+        return count;
+        
     }
 };
