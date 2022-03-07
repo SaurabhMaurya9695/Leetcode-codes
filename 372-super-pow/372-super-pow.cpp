6a5a22a566f908c5316fea7ea40aa0148f21327d
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-03-07 12:35:32 +0530

    Time: 18 ms (32.17%), Space: 11.7 MB (19.07%) - LeetHub

--- 372-super-pow/372-super-pow.cpp
+++ 372-super-pow/372-super-pow.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-    int superPow(int a, vector<int>& b) {
-        long long B=0;
-        for(int i=0 ; i< b.size(); i++){
-            B = ((B*10)+b[i])%1140;
-        }
-        
-        int ans =1;
-        while(B){
-            if(B&1){
-                ans= (ans * 1ll * a)%1337;
-                B--;
-            }else{
-                a =( a* 1ll * a)%1337;
-                B>>=1;
+    
+    long long binExp(long long a,long long b,int m){
+        int ans=1;
+        while(b){
+            if(b&1){
+                ans=(ans*a)%m;
             }
+            a=(a*a)%m;
+            b>>=1;
         }
         return ans;
+    }
+    
+    
+    
+    
+    int superPow(int a, vector<int>& b) {
+        int pow=0;
+        for(auto value:b){
+            pow=(pow*10+value)%1140;
+        }
+        return binExp(a,pow,1337);
     }
 };
