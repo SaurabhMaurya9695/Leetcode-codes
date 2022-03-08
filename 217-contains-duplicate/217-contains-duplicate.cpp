3d725843d6f23dc9612b45a73266bfa26fafc61a
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-03-08 14:08:55 +0530

    Time: 103 ms (77.20%), Space: 46.5 MB (97.11%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
+    Solution() {
+        iostream::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+    }
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin() , nums.end());
-        int flag = 0 , n = nums.size() ;
-        for(int i = 0; i< n - 1 ; i++ )
-        {
-            if(nums[i ] == nums[i+1])
-            {
-                return true ; 
-            }
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1]){return true;}
         }
-        
         return false;
     }
 };
