467668ffa9d7202107bdc822655f57f5350109ef
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-03-08 23:11:59 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (94.28%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
-    #define sort(a) sort(a.begin() , a.end())
     bool isAnagram(string s, string t) {
-        sort(s) ;
-        sort(t);
-        return (s==t) ;
+        int n = s.size() ;
+        int m = t.size() ;
+        if(n != m)
+            return false;
+        vector<int> cnt(26,0) ;
+        for(int i = 0; i< n; i++)
+        {
+            cnt[s[i] - 'a']++;
+            cnt[t[i] - 'a']--;
+        }
+        for(int i = 0 ; i< 26; i++)
+        {
+            if(cnt[i])
+                return false;
+        }
+        return true;
     }
 };
 static int x=[](){
