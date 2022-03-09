dab5e7ddb44dbab4be528bfc0e99b06063484dc8
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-03-09 13:50:01 +0530

    Time: 58 ms (86.49%), Space: 28.5 MB (81.98%) - LeetHub

---
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums)
+    {
+        sort(nums.begin() , nums.end());
+        return nums;
+        
+    }
+};
+static int x=[](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    return 0;
+}();
