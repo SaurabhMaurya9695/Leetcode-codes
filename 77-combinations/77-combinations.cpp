e60194bff7d50b42bcb1006da900fcbd4ff14d21
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-03-10 15:35:12 +0530

    Time: 61 ms (30.85%), Space: 18 MB (36.20%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+void compute(int i, int n, int k, vector<int> &curr, vector<vector<int>> &res) {
+        
+        if (curr.size() == k)  // when our size is equal to the k  then we have to push 
+        {
+            res.push_back(curr); // push 
+            return;
+        }
+        
+        if (i > n) return; // base case ;
+        
+        curr.push_back(i); /// add the i +  1  element 
+		
+        compute(i+1, n, k, curr, res);  // call the function 
+		
+        curr.pop_back();  // remove the last element form the vector
+         
+        compute(i+1, n, k, curr, res); // call the function again 
+    }
+    
+public:
+    vector<vector<int>> combine(int n, int k) {        
+        vector<int> curr; // to store particulr vector 
+        vector<vector<int>> res;  // to store ans ;
+        
+        compute(1, n, k, curr, res);
+        
+        return res;
+    }
+};
