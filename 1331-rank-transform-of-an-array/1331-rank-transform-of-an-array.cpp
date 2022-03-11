7285f864c3757783e3bd63c6c3dd143f777182b0
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-03-11 14:10:02 +0530

    Time: 191 ms (39.21%), Space: 47.4 MB (11.08%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -1,27 +1,61 @@
+// class Solution {
+// public:
+//     vector<int> arrayRankTransform(vector<int>& arr)
+//     {
+//         int n =arr.size() ;
+//         set<int> st ;
+//         for(int &x : arr)
+//         {
+//             st.insert(x);
+//         }
+//         map<int,int>mp;
+//         int rank = 1;
+//         for(auto it : st)
+//         {
+//             mp[it] = rank ;
+//             rank++;
+//         }
+//         for(int i = 0; i< n; i++)
+//         {
+//             arr[i] = mp[arr[i]]; 
+//         }
+//         return arr;
+//     }
+// };
+
+// static int x=[](){
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//     cout.tie(nullptr);
+    
+//     return 0;
+// }();
+
+
+
+
+
+
+
 class Solution {
 public:
-    vector<int> arrayRankTransform(vector<int>& arr)
-    {
-        int n =arr.size() ;
-        set<int> st ;
-        for(int &x : arr)
-        {
-            st.insert(x);
+    vector<int> arrayRankTransform(vector<int>& arr) {
+       set<int> st(arr.begin(),arr.end());
+        unordered_map<int,int> rank;
+        int cur_rank=1;
+        for(auto elem:st){
+            rank[elem]=cur_rank++;
         }
-        map<int,int>mp;
-        int rank = 1;
-        for(auto it : st)
-        {
-            mp[it] = rank ;
-            rank++;
+        
+        vector<int> res;
+        for(int i=0;i<arr.size();i++){
+            res.push_back(rank[arr[i]]);
         }
-        for(int i = 0; i< n; i++)
-        {
-            arr[i] = mp[arr[i]]; 
-        }
-        return arr;
+        
+        return res;
     }
 };
+
 
 static int x=[](){
     ios::sync_with_stdio(false);
