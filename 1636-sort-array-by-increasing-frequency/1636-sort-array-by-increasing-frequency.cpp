d4899a40bc58280b4c819ba9026293097703e5dd
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-03-17 17:13:04 +0530

    Time: 4 ms (95.59%), Space: 11.4 MB (53.83%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -1,28 +1,40 @@
 class Solution {
 public:
      // for comparing the pair second index:
-  static bool comp(pair<int,int>&a,pair<int,int>&b)
-{
-    if(a.first==b.first)
+  static bool comp(pair<int,int>&a , pair<int,int>&b)
     {
-        return a.second>b.second;
+        if(a.first==b.first)
+        {
+            return a.second>b.second;
+        }
+        return a.first<b.first;
     }
-    return a.first<b.first;
-}
-vector<int> frequencySort(vector<int>& nums) {
+vector<int> frequencySort(vector<int>& nums)
+{
     unordered_map<int,int>mp;
     for(auto i:nums)
       mp[i]++; // store frequncy ;
 	  
 	  
-	  // for sorting the array based on their frequency:
-	  
+	  // for sorting the array based on their frequency:  
     vector<pair<int,int>>ans;
     for(auto i:mp)
     {
         ans.push_back({i.second,i.first});
     }
-    sort(ans.begin(),ans.end(),comp);       //comp function for comparision between the pair
+    cout<<"after\n" ;
+    for(int i = 0 ; i< ans.size() ; i++)
+    {
+        cout<< ans[i].first << "-> " << ans[i].second<< "\n" ;
+    }
+    sort(ans.begin(),ans.end(), comp); //comp function for comparision between the pair
+    
+    
+    cout<<"before\n" ;
+    for(int i = 0 ; i< ans.size() ; i++)
+    {
+        cout<< ans[i].first << "-> " << ans[i].second<< "\n" ;
+    }
  
     vector<int>res;
     for(int i=0;i<ans.size();i++)
@@ -35,3 +47,12 @@ vector<int> frequencySort(vector<int>& nums) {
     return res;
 }
 };
+
+//after
+// 1-> 3
+// 3-> 2
+// 2-> 1
+// before
+// 1-> 3
+// 2-> 1
+// 3-> 2
