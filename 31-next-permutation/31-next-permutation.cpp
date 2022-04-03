8543e66dde94e9f7947492f30e93a15b61399b78
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-04-03 20:35:06 +0530

    Time: 13 ms (9.20%), Space: 12.2 MB (37.25%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,8 +1,35 @@
-class Solution {
+class Solution
+{
 public:
-    void nextPermutation(vector<int>& nums) {
+    void nextPermutation(vector<int>& nums) 
+    {
         //method 1 ;
-        next_permutation(nums.begin() , nums.end());
+        // next_permutation(nums.begin() , nums.end()); 
         
+        
+        //method 2;
+//Find the highest index i such that s[i] < s[i+1]. If no such index exists, the permutation is the last permutation.
+// Find the highest index j > i such that s[j] > s[i]. Such a j must exist, since i+1 is such an index.
+// Swap s[i] with s[j].
+// Reverse the order of all of the elements after index i till the last element.
+        int i, j;
+        int n= nums.size();
+        int a=0;
+        for(i=n-2; i>=0; i--){
+            if(nums[i]<nums[i+1]){
+                break;
+            }
+        }
+        if(i<0)
+            reverse(nums.begin(), nums.end());
+        else{
+            for(j=n-1; j>=0; j--){
+                if(nums[i]<nums[j]){
+                    break;
+                }
+            }
+            swap(nums[i], nums[j]);
+            reverse(nums.begin()+i+1, nums.end());
+        }
     }
 };
