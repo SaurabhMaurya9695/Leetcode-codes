1c0d02ce232c08445696f19ba2fa975b05fc9d7e
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-04-04 16:26:42 +0530

    Time: 568 ms (98.77%), Space: 180.1 MB (89.18%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -10,31 +10,70 @@
  */
 class Solution {
 public:
-    ListNode* swapNodes(ListNode* head, int k) {
-        // method 1 using extra space ;
-        vector<int> nums;
+//     ListNode* swapNodes(ListNode* head, int k) {
+//         // method 1 using extra space ;
+//         vector<int> nums;
         
-        //step-1: Convert LL to array
-        while(head != NULL){  //nums: 1 2 3 4 5
-            nums.push_back(head->val);
-            // cout<<head->val<<endl;
-            head = head->next;
-        }
+//         //step-1: Convert LL to array
+//         while(head != NULL){  //nums: 1 2 3 4 5
+//             nums.push_back(head->val);
+//             // cout<<head->val<<endl;
+//             head = head->next;
+//         }
         
-        //step-2: now find those two elements and swap
-        int n = nums.size();
-        cout<<nums[k-1] <<"\t" <<nums[n-k]<<endl;
-        swap(nums[k-1], nums[n-k]);
+//         //step-2: now find those two elements and swap
+//         int n = nums.size();
+//         cout<<nums[k-1] <<"\t" <<nums[n-k]<<endl;
+//         swap(nums[k-1], nums[n-k]);
         
       
-        //step-3: convert back array to LL
-        ListNode* cur = new ListNode(NULL);
-        head = cur;
-        for(int i=0; i<n; i++){
-            cur->next = new ListNode(nums[i]);
-            cur = cur->next;
-        }    
+//         //step-3: convert back array to LL
+//         ListNode* cur = new ListNode(NULL);
+//         head = cur;
+//         for(int i=0; i<n; i++){
+//             cur->next = new ListNode(nums[i]);
+//             cur = cur->next;
+//         }    
         
-        return head->next;
-    }
+//         return head->next;
+//     }
+     ListNode* swapNodes(ListNode* head, int k) 
+     {
+       // method 2 ;
+       // find the kth pos and find n -k + 1 pos .. you just have to swap the values ;
+        ListNode *first, *last , *temp;
+        int i=0;
+        last=head;
+        while(last)
+        { 
+            ++i; 
+            if(i==k)  
+                first=last; 
+            last=last->next;
+        }
+        last=head;
+        k = i-k+1;
+         int j = 0 ;
+        while(last)
+        {
+            ++j;
+            if(j == k)
+            {
+                temp = last;
+            }
+            last = last->next;
+        }
+         swap(temp->val , first->val);
+         return head ;
+     }
+    
+    
 };
+
+static int x=[](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    return 0;
+}();
