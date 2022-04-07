5a82dfa9e5730cfe74991eab80a971791051f3f7
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-04-07 13:46:11 +0530

    Time: 4 ms (34.33%), Space: 7.7 MB (47.62%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -49,12 +49,28 @@ public:
     
     int rob(vector<int>& nums) 
     {
-        int n = nums.size() ;
+        // int n = nums.size() ;
         // return solve( n -1 , nums) ;
-        vector<int> dp(n + 1 , -1);
+        //vector<int> dp(n + 1 , -1);
         // return (solvememo(n - 1 , nums , dp)) ;
         
-        return tab( nums) ;
+        //return tab( nums) ;
+        
+        //space optimization;
+        int n = nums.size();
+        int prev = nums[0];
+        int prev2 = 0;
+            for (int i = 1; i < n; i++)
+            {
+                int take = nums[i];
+                if (i > 1)
+                    take += prev2;
+                int not_take = prev;
+                int curr = max(take, not_take);
+                prev2 = prev;
+                prev = curr;
+            }
+           return prev;
         
     }
 };
