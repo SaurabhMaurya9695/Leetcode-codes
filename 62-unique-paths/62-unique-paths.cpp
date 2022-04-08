3d78e1957b53ff458ce1b931ea23f2cd983c7952
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-04-08 13:06:37 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (57.09%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -27,6 +27,37 @@ public:
         return dp[i][j] = (left + up) ;
     }
 
+    // space optimization
+    int space(int m , int n )
+    {       
+        vector<int> prev(n,0);
+        for(int i=0; i<m; i++)
+        {
+            vector<int> temp(n,0);
+            for(int j=0; j<n; j++)
+            {
+                if(i==0 && j==0)
+                {
+                    temp[j]=1;
+                    continue;
+                }
+
+                int up=0;
+                int left =0;
+
+                if(i>0)
+                    up = prev[j];
+                if(j>0)
+                    left = temp[j-1];
+
+                temp[j] = up + left;
+            }
+            prev = temp;
+    }
+    
+    return prev[n-1];
+        
+    }
     
     int uniquePaths(int m, int n)
     {
@@ -34,30 +65,34 @@ public:
         // return solve(m-1 , n-1) ;   //recursion call;
         
         // return solvememo(m-1 , n-1 , dp); // memorization ;
-        int dp[m][n] ;
         
-        for(int i = 0; i< m ;i++)
-        {
-            for(int j =0 ; j< n ; j++)
-            {
-                if(i == 0 && j== 0)
-                {
-                    dp[i][j] = 1;
-                }
-                else{
-                    int left = 0 , right = 0 ;
-                        if(i > 0) 
-                        {
-                             left  = dp[i-1][j] ;
-                        }
-                        if(j > 0)
-                        {
-                             right = dp[i][j-1];
-                        }
-                        dp[i][j] = left + right ;
-                    }
-            }
-        }
-        return (dp[m-1][n-1]) ;
+        //Tabulation
+        // int dp[m][n] ;        
+        // for(int i = 0; i< m ;i++)
+        // {
+        //     for(int j =0 ; j< n ; j++)
+        //     {
+        //         if(i == 0 && j== 0)
+        //         {
+        //             dp[i][j] = 1;
+        //         }
+        //         else{
+        //             int left = 0 , right = 0 ;
+        //                 if(i > 0) 
+        //                 {
+        //                      left  = dp[i-1][j] ;
+        //                 }
+        //                 if(j > 0)
+        //                 {
+        //                      right = dp[i][j-1];
+        //                 }
+        //                 dp[i][j] = left + right ;
+        //             }
+        //     }
+        // }
+        // return (dp[m-1][n-1]) ;
+        
+        
+        return space(m , n ) ;
     }
 };
