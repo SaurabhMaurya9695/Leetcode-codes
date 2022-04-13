8e22ef68ff12d7d255fddde272abdf417d996f47
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-04-13 14:18:04 +0530

    Time: 12 ms (58.24%), Space: 10.1 MB (47.86%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // Memorization
     int memo(int i, int j, vector<vector<int>>& grid, vector<vector<int>> &dp)
     {
         if (i == 0 && j == 0)  
@@ -15,15 +16,68 @@ public:
 
         return dp[i][j] = min(up, left);
     }
+    
+    
+    // Recursion
+    int solve(int i, int j, vector<vector<int>>& grid)
+    {
+        if (i == 0 && j == 0)  
+            return grid[i][j];
+        if (i < 0 || j < 0)   
+            return 1e9;
 
+        int up = grid[i][j] + solve(i - 1, j, grid);
+        int left = grid[i][j] + solve(i, j - 1, grid);
+
+        return  min(up, left);
+    }
+
+    // tabulation
+    int tab(vector<vector<int>> &grid)
+    {
+       int m = grid.size();
+       int n = grid[0].size();
+
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i == 0 && j == 0) // base case;
+                    dp[i][j] = grid[i][j];
+
+                else
+                {
+                    int up = grid[i][j];
+                    up += (i > 0) ? dp[i - 1][j] : 1e9;
+
+                    int left = grid[i][j];
+                    left += (j > 0) ? dp[i][j - 1] : 1e9;
+
+                    dp[i][j] = min(up, left);
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1];      
+        
+    }
+    
     int minPathSum(vector<vector<int>>& grid)
     {
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<int>> dp(m+1, vector<int>(n + 1, -1));
-
-        return memo(m - 1, n - 1, grid, dp);
+        //return solve(m-1 , n-1 , grid) ;   -> Recursion
+        
+        // memorization
+        // vector<vector<int>> dp(m+1, vector<int>(n + 1, -1));
+        // return memo(m - 1, n - 1, grid, dp);
+        
+        
+        // tabulation ;
+        return tab(grid) ;
     }
 };
     
