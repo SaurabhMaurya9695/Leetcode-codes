42a8be886835977b89a3987ac78fd05da9fdc618
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-05-03 11:53:18 +0530

    Time: 57 ms (22.67%), Space: 27.4 MB (46.57%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -1,54 +1,75 @@
 class Solution {
 public:
-    int findUnsortedSubarray(vector<int>& nums) {
+//     int findUnsortedSubarray(vector<int>& nums) {
         
-        int badL = 0;
-        int badR = 0;
+//         int badL = 0;
+//         int badR = 0;
         
-        int n = nums.size();
+//         int n = nums.size();
         
-        //first number that is not in ascending order
-        for(int i = 1; i < n; i++) {
-            if(nums[i] < nums[i-1]) {
-                badL = i-1;
-                break;
-            }
-        }    
+//         //first number that is not in ascending order
+//         for(int i = 1; i < n; i++) {
+//             if(nums[i] < nums[i-1]) {
+//                 badL = i-1;
+//                 break;
+//             }
+//         }    
         
-        //last number that is not in ascending order
-        for(int i = n - 2; i >= 0; i--) {
-            if(nums[i] > nums[i+1]) {
-                badR = i+1;
-                break;
-            }
-        }
+//         //last number that is not in ascending order
+//         for(int i = n - 2; i >= 0; i--) {
+//             if(nums[i] > nums[i+1]) {
+//                 badR = i+1;
+//                 break;
+//             }
+//         }
         
-        //find the min and max values within the range of not ascending order
-        int mx = INT_MIN;
-        int mn = INT_MAX;
+//         //find the min and max values within the range of not ascending order
+//         int mx = INT_MIN;
+//         int mn = INT_MAX;
 
-        for(int i = badL; i <= badR; i++) {
-            mx = max(nums[i], mx);
-            mn = min(nums[i], mn);
+//         for(int i = badL; i <= badR; i++) {
+//             mx = max(nums[i], mx);
+//             mn = min(nums[i], mn);
+//         }
+        
+//         //already sorted, return 0
+//         if(badL == badR) return 0;
+        
+//         //size of the subarray answer
+//         int ans = badR - badL + 1;
+        
+//         int left = badL;
+//         while(left-1 >= 0 && mn < nums[left-1]) {
+//             ans++;
+//             left--;
+//         }
+        
+//         while(badR+1 < n && mx > nums[badR+1]) {
+//             ans++;
+//             badR++;
+//         }
+        
+//         return ans;
+//     }
+    int findUnsortedSubarray(vector<int>& nums){
+        vector<int> arr(nums.begin() , nums.end()) ;
+        sort(arr.begin() , arr.end()) ;
+        // for(int x : arr)
+        // {
+        //     cout<< x<<" ";
+        // }
+        // cout<< endl;
+        int start = 0;
+        int end = nums.size() - 1;
+        int n = nums.size() ;
+        while(start < n && arr[start] == nums[start])
+        {
+            start++;
         }
-        
-        //already sorted, return 0
-        if(badL == badR) return 0;
-        
-        //size of the subarray answer
-        int ans = badR - badL + 1;
-        
-        int left = badL;
-        while(left-1 >= 0 && mn < nums[left-1]) {
-            ans++;
-            left--;
+        while(end >= 0 && arr[end] == nums[end])
+        {
+            end -- ;
         }
-        
-        while(badR+1 < n && mx > nums[badR+1]) {
-            ans++;
-            badR++;
-        }
-        
-        return ans;
+        return max(0 , end - start + 1) ;
     }
 };
