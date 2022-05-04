f7bb49b4058c1bb11407d5a23d79e77a719bb8ea
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-05-04 21:55:36 +0530

    Time: 260 ms (23.03%), Space: 58 MB (97.14%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,12 +1,36 @@
+// class Solution {
+// public: // Time/Space: O(n); O(n)
+//     int maxOperations(vector<int>& nums, int k) {
+//         unordered_map<int, int> freq;
+//         int ans = 0;
+//         for(auto cur: nums){
+//             int complement = k - cur;
+//             if(freq[complement] > 0){ans++; freq[complement]--;}
+//             else freq[cur]++;
+//         }
+//         return ans;
+//     }
+// };
+
+//
 class Solution {
-public: // Time/Space: O(n); O(n)
+public:
     int maxOperations(vector<int>& nums, int k) {
-        unordered_map<int, int> freq;
-        int ans = 0;
-        for(auto cur: nums){
-            int complement = k - cur;
-            if(freq[complement] > 0){ans++; freq[complement]--;}
-            else freq[cur]++;
+        sort(nums.begin(),nums.end());
+        int ans=0,i=0,j=nums.size()-1;
+        while(i<j)
+        {
+            if(nums[i]+nums[j]==k)
+            {
+                ans++,i++,j--;
+            }
+            else if(nums[i]+nums[j]>k)
+            {
+                j--;
+            }else
+            {
+                i++;
+            }
         }
         return ans;
     }
