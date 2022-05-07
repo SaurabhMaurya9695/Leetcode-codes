1595073de3936915b27c282a3a550533238094bf
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-05-07 09:38:23 +0530

    Time: 128 ms (99.01%), Space: 61.7 MB (60.21%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,27 +1,46 @@
 class Solution {
 public:
-    int maxOperations(vector<int>& arr, int k) 
-    {
+    //int maxOperations(vector<int>& arr, int k) 
+//     {
      
-        sort(arr.begin(), arr.end()) ;
-        int start = 0, end = arr.size() - 1;
-        int cnt = 0;
-        while(start < end)
-        {
-            if(arr[start] + arr[end] == k)
-            {
-                cnt++;
-                start++;
-                    end--;
-            }
-            else if(arr[start] + arr[end] > k)
-            {
-                end-- ;
-            }
-            else{
-                start ++ ;
+//         // sort(arr.begin(), arr.end()) ;
+//         // int start = 0, end = arr.size() - 1;
+//         // int cnt = 0;
+//         // while(start < end)
+//         // {
+//         //     if(arr[start] + arr[end] == k)
+//         //     {
+//         //         cnt++;
+//         //         start++;
+//         //             end--;
+//         //     }
+//         //     else if(arr[start] + arr[end] > k)
+//         //     {
+//         //         end-- ;
+//         //     }
+//         //     else{
+//         //         start ++ ;
+//         //     }
+//         // }
+//         // return (cnt ) ;
+//     }
+    int maxOperations(vector<int>& nums, int k) {
+        unordered_map<int,int> hm;
+        int n = nums.size();
+        int count = 0;
+        
+        for(int i=0;i<n;i++) {
+            if(nums[i]>k) continue;
+            else {
+                if(hm[k-nums[i]] == 0) {
+                    hm[nums[i]]++;
+                }
+                else {
+                    hm[k-nums[i]]--;
+                    count++;
+                }
             }
         }
-        return (cnt ) ;
+        return count;
     }
 };
