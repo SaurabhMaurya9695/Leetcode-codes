3fe64ce38b29dcd6f75a9506aa5cb2fe7c596e07
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-05-10 22:27:00 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (83.34%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
+    // same as combination 2 
         void combinations(int ind , int target, int k ,vector<int> &arr,vector<vector<int>> &ans,vector<int> &ds)
     {
         // for more know in detail make a recursion tree 
         // base case 
         
-        if(target==0 && (k == ds.size()))
+        if(target==0 && (k == ds.size())) // is size of k is equal to ds.size() the we push in ans
         {
             ans.push_back(ds);
             return;
@@ -27,7 +28,7 @@ public:
     }
     
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int> candidates =  {1,2,3,4,5,6,7,8,9} ;
+        vector<int> candidates =  {1,2,3,4,5,6,7,8,9} ; // no is only contain no from 1 to 9 ;
         vector<vector<int>> ans;
         vector<int> ds;
         combinations(0,n , k ,candidates,ans,ds);
