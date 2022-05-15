b2c46aeb05a04e9925d8ee303f739e50387ba10d
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-05-15 10:21:42 +0530

    Time: 20 ms (15.25%), Space: 10.1 MB (48.85%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,96 +1,29 @@
 class Solution {
 public:
-    // Memorization
-    int memo(int i, int j, vector<vector<int>>& grid, vector<vector<int>> &dp)
-    {
-        if (i == 0 && j == 0)  
-            return grid[i][j];
-        if (i < 0 || j < 0)   
-            return 1e9;
-
-        if (dp[i][j] != -1)    
-            return dp[i][j];
-
-        int up = grid[i][j] + memo(i - 1, j, grid, dp);
-        int left = grid[i][j] + memo(i, j - 1, grid, dp);
-
-        return dp[i][j] = min(up, left);
-    }
     
     
-    // Recursion
-    int solve(int i, int j, vector<vector<int>>& grid)
+    int helper(int row, int col, vector<vector<int>> &grid, vector<vector<int>>&dp)
     {
-        if (i == 0 && j == 0)  
-            return grid[i][j];
-        if (i < 0 || j < 0)   
-            return 1e9;
+        // Negative Base Case: Out of the Grid
+        if(row >= grid.size() || col >= grid[0].size()) 
+            return INT_MAX;
+                
+        // Positive Base Case: Destination is Reached
+        if(row == grid.size() - 1 && col == grid[0].size() - 1)
+            return grid[row][col];
+                
+        if(dp[row][col] != -1) return dp[row][col];
 
-        int up = grid[i][j] + solve(i - 1, j, grid);
-        int left = grid[i][j] + solve(i, j - 1, grid);
-
-        return  min(up, left);
-    }
-
-    // tabulation
-    int tab(vector<vector<int>> &grid)
-    {
-       int m = grid.size();
-       int n = grid[0].size();
-
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (i == 0 && j == 0) // base case;
-                    dp[i][j] = grid[i][j];
-
-                else
-                {
-                    int up = grid[i][j];
-                    up += (i > 0) ? dp[i - 1][j] : 1e9;
-
-                    int left = grid[i][j];
-                    left += (j > 0) ? dp[i][j - 1] : 1e9;
-
-                    dp[i][j] = min(up, left);
-                }
-            }
-        }
-
-        return dp[m - 1][n - 1];      
+        int horizontal = helper(row, col + 1, grid, dp); 
+        int vertical = helper(row + 1, col, grid, dp);
         
+        return dp[row][col] = min(horizontal, vertical) + grid[row][col];
     }
     
-    int minPathSum(vector<vector<int>>& grid)
-    {
-        int m = grid.size();
-        int n = grid[0].size();
-
-        //return solve(m-1 , n-1 , grid) ;   -> Recursion
-        
-        // memorization
-        // vector<vector<int>> dp(m+1, vector<int>(n + 1, -1));
-        // return memo(m - 1, n - 1, grid, dp);
-        
-        
-        // tabulation ;
-        return tab(grid) ;
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size() ;
+        int m = grid[0].size() ;
+        vector<vector<int>> dp(n + 1 , vector<int>(m + 1 , -1));
+        return helper(0 , 0 , grid , dp) ;
     }
 };
-    
-//     int bttop(int row , int col , vector<vector<int>> &arr , int m  , int n )
-//     {
-//         //base case ;
-//         if(row  == m && col == n )
-//         {
-//             return arr[0][0] ;
-//         }
-            
-//         int right = bttop(row, col + 1 , arr , m , n) ;
-//         int down = bttop(row + 1 , col , arr ,  m , n);
-//         return min(right , down) + arr[row][col] ;     
-//     }
-    
