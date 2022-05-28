d574d317a05d0aa6e02c215f1b5906f578605b87
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-05-28 21:33:30 +0530

    Time: 23 ms (49.74%), Space: 13.1 MB (29.40%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -22,12 +22,37 @@ public:
         }
         return dp[i][j] ;
     }
+    
+    int tab(vector<int>& a, vector<int>& b)
+    {
+        int n = a.size() ;
+        int m = b.size() ;        
+        
+        vector<vector<int>>dp(n + 1 , vector<int> (m + 1 )) ;
+        
+        for(int i = n - 1 ; i>=0 ; i--)
+        {
+            for(int j = m - 1 ; j>=0 ; j--)
+            {
+                if(a[i] == b[j])
+                {
+                    dp[i][j] = dp[i+1][j+1] + 1;
+                }
+                else{
+                    dp[i][j] = 0 + max(dp[i+1][j] , dp[i][j+1]);
+                }
+            }
+        }
+        return dp[0][0] ;
+    }
     int maxUncrossedLines(vector<int>& a, vector<int>& b) {
         // same code as LCS ;
-        int n = a.size() ;
-        int m = b.size() ;
-        vector<vector<int>> dp(n + 1 , vector<int> (m + 1 ,-1)) ;
-        return solve(0 , 0 , n , m ,dp , a, b ) ;
+        // int n = a.size() ;
+        // int m = b.size() ;
+        // vector<vector<int>> dp(n + 1 , vector<int> (m + 1 ,-1)) ;
+        // return solve(0 , 0 , n , m ,dp , a, b ) ;
+        
+        return tab(a, b) ;
         
     }
 };
