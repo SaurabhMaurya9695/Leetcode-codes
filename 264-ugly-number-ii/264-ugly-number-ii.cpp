fc9334b51870c1747f389e23422ebf6be769df9a
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-05-30 11:05:31 +0530

    Time: 6 ms (87.73%), Space: 9.7 MB (36.96%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.cpp
+++ 264-ugly-number-ii/264-ugly-number-ii.cpp
@@ -2,32 +2,21 @@ class Solution {
 public:
     int nthUglyNumber(int n) 
     {
-        if (n == 1)
-            return 1;
-
-        // Pointers Pointing to Indices not Values
-        int ptr2 = 0, ptr3 = 0, ptr5 = 0;
-
-        vector<int> ugly ;
-        ugly.push_back(1); // to add the 1st ugly no at index 0
-
-        for (int i = 1; i < n; i++) {
-            int a = ugly[(ptr2)] * 2;
-            int b = ugly[(ptr3)] * 3;
-            int c = ugly[(ptr5)] * 5;
-
-            int mini = min(a, min(b, c));
-            ugly.push_back(mini);
-
-            if (mini == a)
-                ptr2++;
-            if (mini == b)
-                ptr3++;
-            if (mini == c)
-                ptr5++;
+        int ptr5 = 0 , ptr2 = 0 , ptr3 = 0 ;
+        vector<int> ans ;
+        ans.push_back(1) ;
+        for(int i = 2 ; i<= n ; i++)
+        {
+            int a = ans[ptr5] * 5 ;
+            int b = ans[ptr3] * 3 ;
+            int c = ans[ptr2] * 2 ;
+            int mini = min(a , min(b , c)) ;
+            ans.push_back(mini);
+            if(mini ==  a) ptr5++ ;
+            if(mini == b ) ptr3++;
+            if(mini == c) ptr2++;
         }
-
-        return ugly[(n - 1)];
+        return ans[n - 1] ;
         
         
     }
