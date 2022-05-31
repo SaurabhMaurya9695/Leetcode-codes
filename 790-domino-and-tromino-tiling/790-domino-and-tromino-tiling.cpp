25bf81c80ea6a10e567993e5e944ab6152c0299c
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-05-31 14:31:37 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (56.24%) - LeetHub

--- 790-domino-and-tromino-tiling/790-domino-and-tromino-tiling.cpp
+++ 790-domino-and-tromino-tiling/790-domino-and-tromino-tiling.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     int numTilings(int n)
     {
-       vector<long long> dp(n +  3 ) ;
-        dp[0] = 1 ;
-        dp[1] = 2;
-        dp[2] = 5 ;
-        for (int i = 3; i < n; i ++)
-        {
-            dp[i] = (2 * dp[i - 1] + dp[i - 3]) % 1000000007;
-        }
-        return dp[n - 1];
+       vector<long long int> dp(1001, 0);
+        // draw a diagram and try to figure out the things 
+        dp[1] = 1;  
+        dp[2] = 2;
+        dp[3] = 5;
+        for(int i = 4; i < 1001; i++)
+            dp[i] = (dp[i - 1] * 2 + dp[i - 3]) % (1000000007);
+        return(dp[n]);
     
     }
 };
