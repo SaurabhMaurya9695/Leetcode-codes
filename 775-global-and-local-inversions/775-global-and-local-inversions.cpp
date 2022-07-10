ac43e8d69eae35244e4976e3e5aff50de6829b05
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-07-10 10:47:15 +0530

    Time: 240 ms (40.18%), Space: 83.4 MB (36.15%) - LeetHub

--- 775-global-and-local-inversions/775-global-and-local-inversions.cpp
+++ 775-global-and-local-inversions/775-global-and-local-inversions.cpp
@@ -1,92 +1,16 @@
 class Solution {
 public:
-    #define ll long long 
-    ll merge(vector<int>&arr, int l, int m, int r)
-    {
-         // Your code here
-         ll n1 = m - l + 1;
-         ll n2= r - m;
-         ll arr1[n1] , arr2[n2];
-         for(int i=0;i<n1;i++)
-         {
-             arr1[i]=arr[i+l];
-         }
-         for(int i=0;i<n2;i++)
-         {
-             arr2[i]=arr[i+m+1];
-         }
-        
-          ll c=0;
-          ll i, j, k;
-              i = 0;
-              j = 0;
-              k = l;
-        
-        // cnt inversion 
-        while(i<n1 && j<n2)
-        {
-            if(arr1[i]> arr2[j])
-            {
-                c+= n1 - i ;
-                j++;
-            }
-            else
-                i++;
-        }
-        
-            i=0,j=0;
-        
-           while (i < n1 && j < n2) {
-            if (arr1[i] <= arr2[j]) {
-              arr[k] = arr1[i];
-              i++;
-            } else {
-              arr[k] = arr2[j];
-              j++;
-            }
-            k++;
-          }
-           while (i < n1) {
-            arr[k] = arr1[i];
-            i++;
-            k++;
-          }
-        
-          while (j < n2) {
-            arr[k] = arr2[j];
-            j++;
-            k++;
-          }
-        return c;
-    }
-
-    ll mergeSort(vector<int>& arr, int l, int r)
-    {
-        //code here
-        ll c=0;
-        if(l<r)
-        {
-            int m=(l+r)/2;
-            c+=mergeSort(arr,l,m); // ans from left ;
-            c+=mergeSort(arr,m+1,r); // ans from right ;
-            c+=merge(arr,l,m,r); // ans after merging
-        }
-        return c;
-    }
-    
     bool isIdealPermutation(vector<int>& nums) {
-        ll n = nums.size() ;
-        ll local = 0;
-        ios_base::sync_with_stdio(false);cin.tie(NULL);
-        for(int i = 0 ; i< n - 1 ; i++)
+        for(int i = 0 ; i< nums.size()  ; i++)
         {
-            if(nums[i] > nums[i + 1])
-            {
-                local ++ ;
-            }
+            if(abs(nums[i] - i) > 1) return false;
         }
-        
-        
-        return ( mergeSort(nums , 0 , n  - 1) == local ) ;
+        return true;
     }
 };
+
+// if all numbers in asending order 
+// 0 1 2 3  -> no inversion ;
+// 3 2 1 0 -> max number of invesion ; 
+
+// we have to check that the diff between i and i - 1 < 1 ;
