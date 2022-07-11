bbc85e0664b193dd83905d6e251e199c5c5e3c5e
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-07-11 11:36:38 +0530

    Time: 163 ms (83.15%), Space: 83.2 MB (91.63%) - LeetHub

--- 775-global-and-local-inversions/775-global-and-local-inversions.cpp
+++ 775-global-and-local-inversions/775-global-and-local-inversions.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-    bool isIdealPermutation(vector<int>& nums) {
-        for(int i = 0 ; i< nums.size()  ; i++)
+    bool isIdealPermutation(vector<int>& arr) {
+        int n = arr.size() ;
+        for(int i = 0 ; i<n ; i++)
         {
-            if(abs(nums[i] - i) > 1) return false;
+            if(abs(i - arr[i] ) > 1)
+            {
+                return false;
+            }
         }
         return true;
     }
 };
-
-// if all numbers in asending order 
-// 0 1 2 3  -> no inversion ;
-// 3 2 1 0 -> max number of invesion ; 
-
-// we have to check that the diff between i and i - 1 < 1 ;
