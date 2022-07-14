89ff603bb4801a2adad0e089b97aaeff66bcafe4
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-07-14 23:12:29 +0530

    Time: 11 ms (17.04%), Space: 8.7 MB (99.41%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -1,57 +1,51 @@
 class Solution {
 public:
-    int findPeakElement(vector<int>& arr) {
-        int n = arr.size() ;
+    int findPeakElement(vector<int>& nums) {
+        
+        int start =0 ;
+        int end = nums.size() - 1;
+        int n = nums.size() ;
         if(n == 1) return 0;
-
-        int start = 0 ;
-        int end = n - 1;
-        int res = -1 ;
+        if(n == 2)
+        {
+            if(nums[0] > nums[1]) return 0;
+            else  return 1;
+        }
+        
+        int res = -1;
         while(start <= end)
         {
-            int mid = start + (end - start) / 2 ;
-            if(mid > 0 &&  mid < n - 1)
+            int mid = (start + (end - start) / 2) ;
+            if(mid > 0 &&  mid < n-1) // means mid can't be 0 or n - 1;
             {
-                if(arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1])
-                {
-                    res = mid;
-                    break;
-                }
-                else if(arr[mid + 1] > arr[mid])
-                {
-                    start = mid + 1;
-                }
-                else{
-                    end = mid - 1;
-                }
+                if(nums[mid] > nums[mid +1] && nums[mid] > nums[mid -1])
+                    {
+                        res = mid ;
+                        break;
+                    }
+                    else if(nums[mid+1] > nums[mid]) // 7 8 9
+                    {
+                        start = mid + 1;
+                    }
+                    else{
+                        end = mid - 1;
+                    }
             }
-             // mid == 0 ;
-            if( mid == 0)
+            else if(mid == 0) //only two elemnt is there at starting;
             {
-                if(arr[0] > arr[1])
-                {
-                    res = 0;
-                    break; 
-                }
-                else{
-                    res = 1 ;
-                    break;
-                }
+                res = (nums[0] > nums[1]) ? 0 : 1 ;
+                break;
             }
-            if(mid ==  n- 1)
-            {
-                if(arr[n-1] > arr[n - 2])
-                {
-                    res =  n-1;
-                    break;
-                }
-                else{
-                    res =  n-2;
-                    break;
-                }
+            else if(mid == n - 1){
+                res = (nums[n-1] > nums[n-2] ) ? n-1 : n-2;
+                break;
+                
             }
+                
             
         }
-        return res ;
+        return res;
+        
+        
     }
 };
