80b5ba809ac4add2081a4e65f9a5f26821c84b18
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-07-24 21:12:11 +0530

    Time: 7 ms (96.46%), Space: 11.9 MB (61.75%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
@@ -1,47 +1,55 @@
 class Solution {
 public:
-    int evalRPN(vector<string>& A) {
-         stack<int>s;
-    for(string p:A)
-    {
-        if(p!="+" and p!="*" and p!="-" and p!="/")
+    int evalRPN(vector<string>& s) {
+        int n = s.size();
+        stack<int> stk;
+        for(int i = 0 ; i< n ; i++)
         {
-            s.push(stoi(p));
-        }
-        else
-        {
-            if(p=="+")
+            if(s[i] != "+" && s[i] != "-" && s[i] != "/" && s[i] != "*")
             {
-                int b=s.top();s.pop();
-                int a=s.top();s.pop();
-                int c = a+b;
-                s.push(c);
+                stk.push(stoi(s[i]));
             }
-            else if (p=="-")
-            {
-                int b=s.top();s.pop();
-                int a=s.top();s.pop();
-                int c = a-b;
-                s.push(c);
+            else {
+                if(s[i] == "+")
+                {
+                    int a = stk.top();
+                    stk.pop();
+                    int b = stk.top();
+                    stk.pop();
+                    int c = b + a;
+                    stk.push(c);
+                }
+                else if(s[i] == "-")
+                {
+                    int a = stk.top();
+                    stk.pop();
+                    int b = stk.top();
+                    stk.pop();
+                    int  c = b - a;
+                    stk.push(c);
+                }
+                else if(s[i] == "*")
+                {
+                    int a = stk.top();
+                    stk.pop();
+                    int b = stk.top();
+                    stk.pop();
+                    int  c = b * a;
+                    stk.push(c);
+                }
+                else if(s[i] == "/")
+                {
+                    int a = stk.top();
+                    stk.pop();
+                    int b = stk.top();
+                    stk.pop();
+                    int  c = b / a;
+                    stk.push(c);
+                }
             }
-            else if (p=="*")
-            {
-                int b=s.top();s.pop();
-                int a=s.top();s.pop();
-                int c = a*b;
-                s.push(c);
-            }
-            else
-            {
-                int b=s.top();s.pop();
-                int a=s.top();s.pop();
-                int c = a/b;
-                s.push(c);
-            }
-            
-        }
-    }
-    return s.top();
         
+        }
+     return stk.top();
     }
+
 };
