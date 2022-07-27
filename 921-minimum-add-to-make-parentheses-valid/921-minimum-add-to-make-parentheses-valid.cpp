2e02104bb7f20244570f25bbf753e227c7ff3040
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-07-27 16:41:42 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (97.50%) - LeetHub

--- 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.cpp
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,26 +1,56 @@
 class Solution {
 public:
-    int minAddToMakeValid(string s) {
-        int cnt = 0;
-        stack<char> stk;
-        int n = s.size();
-        for(int i = 0 ; i <  n ;i++)
+//     int minAddToMakeValid(string s) {
+//         int cnt = 0;
+//         stack<char> stk;
+//         int n = s.size();
+//         for(int i = 0 ; i <  n ;i++)
+//         {
+//             if(s[i] == ')')
+//             {
+//                 if(stk.size() > 0 && stk.top() == '(')
+//                 {
+//                     stk.pop();
+//                 }
+//                 else {
+//                     stk.push(s[i]);
+//                 }
+//             }
+//             else{
+//                 stk.push(s[i]);
+//             }
+//         }
+//         // return stk.size() ;
+//         while(!stk.empty())
+//         {
+//             stk.pop();
+//             cnt++;
+//         }
+//         return cnt;
+        
+//     }
+    
+    int minAddToMakeValid(string s) 
+    {
+        
+        // time -> o(n);
+        // space -> o(1);
+        int open = 0 , close = 0 , ans = 0;
+        for(auto x : s)
         {
-            if(s[i] == ')')
+            if(x == '(')
             {
-                if(stk.size() > 0 && stk.top() == '(')
-                {
-                    stk.pop();
-                }
-                else {
-                    stk.push(s[i]);
-                }
+                open++;
             }
             else{
-                stk.push(s[i]);
+                close++;
+            }
+            if(close > open)
+            {
+                ans++;
+                open++;
             }
         }
-        return stk.size() ;
-        
+        return ans + (abs ( open -close));
     }
 };
