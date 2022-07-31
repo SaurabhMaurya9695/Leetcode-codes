7f8b33a3aef2f5eb3d3f68b0d5e4382205019231
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-07-31 07:49:06 +0530

    Time: 91 ms (98.85%), Space: 31.2 MB (80.35%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,22 +1,62 @@
-class Solution {
+// class Solution {
+// public:
+    // int minSwaps(string s) {
+    //     int n = s.size() ;
+    //     int cnt =0;
+    //     stack<char> stk;
+    //     for(int i = 0; i< n ;i++)
+    //     {
+    //         if(s[i] == '[')
+    //         {
+    //             stk.push(s[i]);
+    //         }
+    //         else {
+    //             if(stk.size()  > 0 && stk.top() == '[')
+    //             {
+    //                 stk.pop();
+    //             }
+    //             else{
+    //                 stk.push(s[i]);
+    //             }
+    //         }
+    //     }
+    //     int b = stk.size() / 2;
+    //     int ans = (b % 2 == 1) ? b/2 + 1 : b /2;
+    //     return ans ;
+    // }
+// };
+
+    class Solution {
 public:
     int minSwaps(string s) {
-        stack<char> st;
-        int count = 0;
-        //remove all balanced paranthesis
-        for(int i = 0 ; i < s.length() ; i++){
-            if(s[i] == '[')
-                st.push(s[i]);
-            else
-            {
-                if(!st.empty() && st.top() == '[')
-                    st.pop();
-                else
-                    count++;
-            }
+        // int start=0,end=s.size()-1,open=0,close=0,ans=0;
+        // while(start<end){
+        //     if(s[start]==']'){
+        //         open++;
+        //     } else {
+        //         close++;
+        //     }
+        //     if(open>close){
+        //         while(s[end]!='['){
+        //             end--;
+        //         }
+        //         swap(s[start],s[end]);
+        //         ans++;
+        //         open--;
+        //         close++;
+        //     }
+        //     start++;
+        // }
+        // return ans;
+        int close=0 , maxClose=0;
+        for(char ch:s)
+        {
+            if(ch==']')
+                close++;
+            else if(ch == '[')
+                close--;
+            maxClose=max(maxClose,close);
         }
-        //remaining only unbalanced paranthesis
-        int ans = (count + 1) / 2;
-        return ans;
+        return (maxClose + 1)/2;
     }
 };
