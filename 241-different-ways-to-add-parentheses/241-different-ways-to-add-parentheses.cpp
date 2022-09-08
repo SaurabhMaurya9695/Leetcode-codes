5f037a147bac632163d5411f53af4e077e8cf549
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-09-08 16:16:31 +0530

    Time: 14 ms (11.74%), Space: 10.9 MB (63.42%) - LeetHub

--- 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.cpp
+++ 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.cpp
@@ -1,32 +1,48 @@
 class Solution {
 public:
-    int operate(int x, int y, char op) 
+    
+    int operate(int x  , int y , char c)
     {
-        if(op == '+') return x + y;
-        if(op == '-') return x - y;
-        if(op == '*') return x * y;
-        return 0;
+        if(c == '*') return x * y;
+        else if(c == '+') return x + y;
+        else{
+            return x - y;
+        }
     }
-
-    vector<int> diffWaysToCompute(string expression) {
-        vector<int> results;
-        bool isEntirelyNumber = true;
-
-        for (int i = 0; i < expression.length(); i++) {
-            if (!isdigit(expression[i])) 
+    
+    vector<int> solve(string s)
+    {
+        vector<int> ans;
+        int n = s.size() ;
+        bool allarenumbers = true;
+        for(int i = 0 ; i< s.size() ; i++)
+        {
+            if(s[i] == '*' || s[i] == '+' || s[i] == '-')
             {
-                isEntirelyNumber = false;
-
-                vector<int> left = diffWaysToCompute(expression.substr(0, i));
-                vector<int> right = diffWaysToCompute(expression.substr(i + 1));
-
-                for (int x: left) {
-                    for (int y: right) results.push_back(operate(x, y, expression[i]));
+                allarenumbers = false;
+                string left = s.substr(0 , i); // 0.. i-1
+                string right = s.substr(i + 1 , n) ;
+                vector<int> leftv = solve(left) ;
+                vector<int> rightv = solve(right);
+                
+                for(int k = 0 ; k < leftv.size() ; k++)
+                {
+                    for(int j = 0 ; j< rightv.size() ; j ++)
+                    {
+                        ans.push_back(operate(leftv[k] , rightv[j] , s[i]));
+                    }
                 }
             }
         }
-
-        if (isEntirelyNumber) results.push_back(stoi(expression));
-        return results;
+        if(allarenumbers == true)
+        {
+            ans.push_back(stoi(s));
+        }
+        return ans ;
+        
+    }
+    vector<int> diffWaysToCompute(string expression) 
+    {
+        return solve(expression) ;
     }
 };
