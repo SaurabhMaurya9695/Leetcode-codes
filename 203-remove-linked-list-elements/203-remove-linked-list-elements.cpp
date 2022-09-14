7099e39cb83fdbb9cd9b8119377af97fdc2105c6
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-09-14 12:57:02 +0530

    Time: 52 ms (18.15%), Space: 15.4 MB (10.44%) - LeetHub

--- 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
@@ -11,26 +11,34 @@
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) 
-    {
-        if(head == NULL) {
-            return NULL;
-        }
-        ListNode * dummy = new ListNode(-1);
-        dummy -> next = head;
-        ListNode * tail = dummy;
-        while(tail != NULL &&  tail->next != NULL)
-        {
-            if(tail->next->val ==  val)
-            {
-                ListNode * temp = tail->next;
-                tail->next = temp->next;
-                delete temp;
-            }
-           else{
-               tail = tail->next;
-           }
-        }
-           return dummy->next;
+    // {
+    //     if(head == NULL) {
+    //         return NULL;
+    //     }
+    //     ListNode * dummy = new ListNode(-1);
+    //     dummy -> next = head;
+    //     ListNode * tail = dummy;
+    //     while(tail != NULL &&  tail->next != NULL)
+    //     {
+    //         if(tail->next->val ==  val)
+    //         {
+    //             ListNode * temp = tail->next;
+    //             tail->next = temp->next;
+    //             delete temp;
+    //         }
+    //        else{
+    //            tail = tail->next;
+    //        }
+    //     }
+    //        return dummy->next;
         
+    {
+        if(head == NULL) return head;
+        head->next = removeElements(head->next , val);
+        if(head->val == val)
+            return head->next;
+        else{
+            return head;
+        }
     }
 };
