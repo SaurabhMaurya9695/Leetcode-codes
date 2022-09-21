045028fe325d1fa5f0ea5572cc3ffa9484b65b43
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-09-21 15:33:47 +0530

    Time: 39 ms (5.10%), Space: 10 MB (8.69%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -1,25 +1,17 @@
 
 class Solution {
 public:
-    ListNode *detectCycle(ListNode *head) 
-    {
-        ListNode * hare = head;
-        ListNode * turtle = head;
-        
-        while (hare != NULL && hare->next != NULL) {
-            hare = hare->next->next;
-            turtle = turtle->next;
-            
-            if (hare == turtle) {
-                turtle = head;
-                while (hare != turtle) {
-                    hare = hare->next;
-                    turtle = turtle->next;
-                }
-                return turtle;
+    ListNode *detectCycle(ListNode *head) {
+        unordered_map<ListNode * , bool > ump;
+        ListNode * curr = head;
+        while(curr != NULL)
+        {
+            if(ump.find(curr) != ump.end()) return curr;
+            else{
+                ump[curr] = true;
             }
+            curr = curr -> next;
         }
-        return NULL;
-        
+        return nullptr;  
     }
 };
