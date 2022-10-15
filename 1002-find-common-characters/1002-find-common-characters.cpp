ac6c6ec04accab0d3c1cd3792519a875beda86ab
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-10-15 14:56:09 +0530

    Time: 47 ms (13.03%), Space: 12.1 MB (10.21%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.cpp
+++ 1002-find-common-characters/1002-find-common-characters.cpp
@@ -2,44 +2,26 @@ class Solution {
 public:
     vector<string> commonChars(vector<string>& s) 
     {
-        int n = s.size() ;
+        string temp = s[0] ;
         map<char , int > mp ;
-        string temp = s[0];
-        for(auto x : temp)
-        {
-            mp[x]++; 
-        }
-        map<char , int > mpp;
+        for(auto x : temp) mp[x]++;
         for(int i = 1 ; i< s.size() ; i++)
         {
-            map<char , int > mpp;
             string ss = s[i];
-            for(auto x : ss)
+            map<char , int > mpp ;
+            for(auto y : ss) mpp[y] ++;
+            for(auto xx : s[0])
             {
-                mpp[x]++;
+                mp[xx] = min(mp[xx] , mpp[xx]) ;
             }
-            // check in both ;
-            for(auto y : s[0] )
-            {
-                if(mp[y] != mpp[y]) // finds in second ;
-                {
-                    mp[y] = min(mp[y] , mpp[y]);
-                }
-            }
-                
         }
-        // for(auto x:  mp)
-        // {
-        //     cout << x.first << " " << x.second << " ";
-        // }
-        // cout<< endl;
         vector<string> ans ;
-        for(auto ch:s[0])
+        for(auto x : s[0] )
         {
-            if(mp[ch]>0)
+            if(mp[x] > 0)
             {
-                ans.push_back(string(1,ch));
-                mp[ch]--;
+                ans.push_back(string(1 , x));
+                mp[x] --;
             }
         }
         return ans;
