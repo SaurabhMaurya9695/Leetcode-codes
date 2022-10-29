77787b5e69e97a12521b16069fa1d58c246d96cb
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-10-29 10:16:05 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (38.19%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -13,25 +13,65 @@ public:
         
 //     }
     
-    int solve(int n , vector<int>&dp)
-    {
-        if(n == 0 || n == 1)
-        {
-            return n;
-        }
-        if(dp[n] != -1)
-        {
-            return dp[n];
-        }
+                        // 2 - Memorization 
+//     int solve(int n , vector<int>&dp)
+//     {
+//         if(n == 0 || n == 1)
+//         {
+//             return n;
+//         }
+//         if(dp[n] != -1)
+//         {
+//             return dp[n];
+//         }
         
-        dp[n] =  solve(n - 1 , dp) + solve(n - 2 , dp);
-        return dp[n];
+//         dp[n] =  solve(n - 1 , dp) + solve(n - 2 , dp);
+//         return dp[n];
         
-    }
+//     }
     
+//     int fib(int n)
+//     {
+//         vector<int> dp(n + 1 , -1);
+//         return solve(n , dp);
+//     }
+    
+                        // 3 - Tabulation 
+//     int tab(vector<int> &dp ,int n)
+//     {
+//         dp[0] = 0;
+//         dp[1] = 1;
+//         for(int i = 2 ; i<= n; i++)
+//         {
+//             dp[i] = dp[i-1] + dp[i-2];
+//         }
+//         return dp[n];
+        
+        
+//     }
+//     int fib(int n) 
+//     {   
+//         // tabulation
+//         vector<int> dp(n+1);
+//         return tab(dp , n) ;
+//     }
+    
+                // 4- space optimization
     int fib(int n)
     {
-        vector<int> dp(n + 1 , -1);
-        return solve(n , dp);
+        if(n == 0 || n == 1) return n ;
+        int prev1 = 1 , prev2 = 0 ;
+        int curr = 0;
+        for(int i = 2 ; i <= n ; i ++)
+        {
+            curr = prev1 + prev2 ;
+            prev2  = prev1 ;
+            prev1 = curr ;
+        }
+        return prev1;
     }
 };
+
+
+
+
