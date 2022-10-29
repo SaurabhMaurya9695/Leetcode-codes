d53f6daa2b035cde562887c998ba15973f65ab47
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-10-29 11:10:57 +0530

    Time: 0 ms (100.00%), Space: 6 MB (53.21%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -34,37 +34,36 @@ public:
 //     }
     
                     // 3 - tabulation 
+    int tribonacci(int n) 
+    {
+        vector<int> dp(38);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+        return dp[n];
+    }
+                // 4 - space optimization 
     // int tribonacci(int n)
     // {
-    //     vector<int> dp(n + 1);
-    //     dp[0] = 0 ;
-    //     dp[1] = 1;
-    //     dp[2] = 1;
-    //     for(int i = 3 ; i<= n ; i++)
+    //     if(n == 0) return 0;
+    //     if(n == 1) return 1 ;
+    //     if( n == 2) return 1;
+    //     int prev1 = 0 ;
+    //     int prev2 = 1 ;
+    //     int prev3 = 1 ;
+    //     int curr = 0 ;
+    //     for(int i = 3 ; i <= n ; i++)
     //     {
-    //         dp[i] = dp[ i - 1] + dp[i - 2] + dp[i - 3];
+    //         curr = prev1 + prev2 +  prev3;
+    //         prev1 = prev2 ;
+    //         prev2 = prev3 ;
+    //         prev3 = curr ;
     //     }
-    //     return dp[n];
+    //     return curr;
     // }
-                // 4 - space optimization 
-    int tribonacci(int n)
-    {
-        if(n == 0) return 0;
-        if(n == 1) return 1 ;
-        if( n == 2) return 1;
-        int prev1 = 0 ;
-        int prev2 = 1 ;
-        int prev3 = 1 ;
-        int curr = 0 ;
-        for(int i = 3 ; i <= n ; i++)
-        {
-            curr = prev1 + prev2 +  prev3;
-            prev1 = prev2 ;
-            prev2 = prev3 ;
-            prev3 = curr ;
-        }
-        return curr;
-    }
 };
 
 
