393cc14abfc207631df40258189368b9878ca83a
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-10-29 10:56:17 +0530

    Time: 4 ms (21.64%), Space: 6 MB (53.21%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -10,24 +10,64 @@ public:
     //         return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
     //     }
     // }
-    int solve(vector<int>&dp  , int n)
+    
+                    // Memorization
+//     int solve(vector<int>&dp  , int n)
+//     {
+//         if(n == 0) return 0;
+//         if(n == 1) return 1 ;
+//         if( n == 2) return 1;
+//         if(dp[n] != -1)
+//         {
+//             return dp[n];
+//         }
+        
+//         dp[n] = solve(dp ,  n - 1) + solve(dp ,  n - 2) + solve(dp , n - 3);
+//         return dp[n];
+        
+//     }
+    
+//     int tribonacci(int n)
+//     {
+//         vector<int> dp(n + 1 , -1);
+//         return solve(dp ,  n);
+//     }
+    
+                    // 3 - tabulation 
+    // int tribonacci(int n)
+    // {
+    //     vector<int> dp(n + 1);
+    //     dp[0] = 0 ;
+    //     dp[1] = 1;
+    //     dp[2] = 1;
+    //     for(int i = 3 ; i<= n ; i++)
+    //     {
+    //         dp[i] = dp[ i - 1] + dp[i - 2] + dp[i - 3];
+    //     }
+    //     return dp[n];
+    // }
+                // 4 - space optimization 
+    int tribonacci(int n)
     {
         if(n == 0) return 0;
         if(n == 1) return 1 ;
         if( n == 2) return 1;
-        if(dp[n] != -1)
+        int prev1 = 0 ;
+        int prev2 = 1 ;
+        int prev3 = 1 ;
+        int curr = 0 ;
+        for(int i = 3 ; i <= n ; i++)
         {
-            return dp[n];
+            curr = prev1 + prev2 +  prev3;
+            prev1 = prev2 ;
+            prev2 = prev3 ;
+            prev3 = curr ;
         }
-        
-        dp[n] = solve(dp ,  n - 1) + solve(dp ,  n - 2) + solve(dp , n - 3);
-        return dp[n];
-        
-    }
-    
-    int tribonacci(int n)
-    {
-        vector<int> dp(n + 1 , -1);
-        return solve(dp ,  n);
+        return curr;
     }
 };
+
+
+
+
+
