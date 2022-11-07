a3b037d38ce4b3b045bdc208cdae76b5d7025c5d
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-11-07 10:50:27 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (15.40%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
     
-    int solve(int idx , int n , vector<int>&dp)
-    {
-        if(idx > n) return 0 ; // base case ;
-        if(idx ==  n ) return 1;
-        if(dp[idx] != -1) return dp[idx]; // lookup
-        int left = solve(idx + 1 , n , dp) ;
-        int right = solve(idx + 2 , n , dp);
-        return dp[idx] = left + right ;
-    }
+            // Recursion + Memorization -> top down 
+    // int solve(int idx , int n , vector<int>&dp)
+    // {
+    //     if(idx > n) return 0 ; // base case ;
+    //     if(idx ==  n ) return 1;
+    //     if(dp[idx] != -1) return dp[idx]; // lookup
+    //     int left = solve(idx + 1 , n , dp) ;
+    //     int right = solve(idx + 2 , n , dp);
+    //     return dp[idx] = left + right ;
+    // }
+    
+    
     int climbStairs(int n) {
-        vector<int> dp(n + 1 , -1);
-        return solve(0 , n , dp) ;
+        // vector<int> dp(n + 1 , -1);
+        //Tabulation 
+        
+        vector<int> dp(n + 1) ;
+        if(n == 0 || n == 1) return 1;
+        dp[0] = 1;
+        dp[1] = 1 ;
+        dp[2] = 2 ;
+        for(int i = 3 ; i <= n; i++)
+        {
+            dp[i ] = dp[i - 1] + dp[i - 2];
+        }
+        // return dp[n];
+        for(auto x : dp)
+        {
+            cout<< x << " ";
+        }
+        return dp[n];
         
     }
 };
