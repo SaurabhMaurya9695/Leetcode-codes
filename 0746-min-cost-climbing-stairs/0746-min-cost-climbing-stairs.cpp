3124a3c3e327701a115f2058cf70c8754a7ecff2
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2022-11-07 13:27:51 +0530

    Time: 15 ms (44.94%), Space: 13.9 MB (30.97%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,27 +1,42 @@
 class Solution {
 public:
     
-    int solve(int sr , int dc , vector<int> cost , vector<int>&dp)
-    {
-        if(sr == dc) return cost[sr];
-        if(sr > dc) return  0;
-        // lookup table ;
-        if(dp[sr] != -1)
-        {
-            return dp[sr];
-        }
-        int left = solve(sr + 1 ,dc , cost , dp) + cost[sr];
-        int right = solve(sr + 2 ,dc , cost , dp) + cost[sr];
-        return dp[sr] = min(left , right) ;
-    }
+    //recursion  + Memoriation
+    // int solve(int sr , int dc , vector<int> cost , vector<int>&dp)
+    // {
+    //     if(sr == dc) return cost[sr];
+    //     if(sr > dc) return  0;
+    //     // lookup table ;
+    //     if(dp[sr] != -1)
+    //     {
+    //         return dp[sr];
+    //     }
+    //     int left = solve(sr + 1 ,dc , cost , dp) + cost[sr];
+    //     int right = solve(sr + 2 ,dc , cost , dp) + cost[sr];
+    //     return dp[sr] = min(left , right) ;
+    // }
     
     
     int minCostClimbingStairs(vector<int>& cost) {
-        int n = cost.size() ;
+        // int n = cost.size() ;
         
-        vector<int> dp(n +  1 , -1);
-        int zidx = solve(0 , n - 1 , cost , dp) ;
-        int fidx = solve( 1 , n - 1 , cost , dp);
-        return min(zidx , fidx);
+        // vector<int> dp(n +  1 , -1);
+        // int zidx = solve(0 , n - 1 , cost , dp) ;
+        // int fidx = solve( 1 , n - 1 , cost , dp);
+         // return min(zidx , fidx);
+        
+                        // tabulation
+        int n=cost.size();
+        vector<int> dp(n+1,0);
+        dp[n-1]=cost[n-1];
+        for(int i=n-2;i>=0;i--)
+
+        {
+            dp[i]=min(cost[i]+dp[i+1],cost[i]+dp[i+2]);
+        }
+        return min(dp[0],dp[1]);
     }
+    
 };
+
+
