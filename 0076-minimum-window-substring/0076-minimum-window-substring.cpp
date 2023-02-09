5c8be8f0e06f355a63ca94911f96e287773ed8e2
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2023-02-09 21:55:56 +0530

    Time: 33 ms (50.68%), Space: 7.8 MB (95.57%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,41 +1,44 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-      int i=0;
-      int j=0;
-      int mini=INT_MAX;
-      unordered_map<char,int>m; //making a map to store count of each character of "t" string
-        for(int i=0;i<t.size();i++){
-            m[t[i]]++;          //By traversing each element of t, set count of each character 
-        }
-        
-        int count =m.size();
-        string ans="";
-        while(j<s.size()){
-        //Finding element of t in string s and if element is there decrese the element count 
-                if(m.find(s[j])!=m.end()){
-                m[s[j]]--;        
-                if(m[s[j]]==0){
-                    count--;
+        long long n = s.size() , m = t.size() ;
+        unordered_map<char , long long > mp ;
+        for(auto x : t) mp[x] ++ ;
+        long long size = mp.size() ;
+        long long ans = INT_MAX ;
+        long long i = 0  , j = 0 ;
+        int lsti = 0 ;
+        string res;
+        while(j < n)
+        {
+            if(mp.find(s[j]) != mp.end()){
+                mp[s[j]]--;
+                if(mp[s[j]] == 0) {
+                    size -- ;
                 }
             }
-            
-//As soon as we get count =0 we are approaching our answer , then we will find here our min substring having string t elements
-            while(count==0){
-                if(mini>j-i+1){
-                    mini=min(mini,j-i+1);
-                    ans=s.substr(i,j-i+1);
-                }
-                if(m.find(s[i])!=m.end()){
-                    m[s[i]]++;
-                    if(m[s[i]]==1){
-                        count++;
+            if(size == 0)
+            {
+                while(size == 0)
+                {
+                    if(ans > j - i + 1)
+                    {
+                        ans = min(ans , j - i  + 1);
+                        lsti = i;
+                        // res = s.substr(i , j - i  + 1) ;
                     }
+                    if(mp.find(s[i]) != mp.end())
+                    {
+                        mp[s[i]]++;
+                        if(mp[s[i]] == 1) size ++;
+                    }
+                    i++;
                 }
-                i++;
             }
-            j++;
+            j ++ ;
+            
         }
-        return ans;
+        return (ans == INT_MAX) ? "" : s.substr(lsti , ans);
+        
     }
 };
