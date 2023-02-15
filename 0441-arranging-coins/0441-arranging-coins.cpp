3cca8444037e0068eab594d93380cdcf710ce710
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2023-02-15 22:13:07 +0530

    Time: 4 ms (75.06%), Space: 6 MB (8.47%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,29 +1,44 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-            // o(N) ;
+                        // o(N) ;
         // int coin = 1 ;
         // while(n > 0){ // 2
         //     n -= coin; // 2 - 3 => -1
         //     coin ++ ; // 4    
         // }
         // return coin - 2;
+                        
+                        //o(logn);
         
-        int start = 1 , end = n ;
-        int ans = 0 ;
-        while(start <= end)
-        {
-            long long mid = start + (end - start) / 2;
-            long long tcoins = (mid * (mid +  1)) / 2;
-            if(tcoins <= n){
-                ans = mid ;
-                start =  mid + 1;
-            }
-            else{
-                end = mid -1;
-            }
+//         int start = 1 , end = n ;
+//         int ans = 0 ;
+//         while(start <= end)
+//         {
+//             long long mid = start + (end - start) / 2;
+//             long long tcoins = (mid * (mid +  1)) / 2;
+//             if(tcoins <= n){
+//                 ans = mid ;
+//                 start =  mid + 1;
+//             }
+//             else{
+//                 end = mid -1;
+//             }
             
-        }
-        return ans ;
+//         }
+//         return ans ;
+        
+                        //Maths
+        
+        return (int)((-1 + sqrt(1 + 8 *1ll * n)) / 2) ; 
     }
 };
+
+
+
+
+
+
+
+
+
