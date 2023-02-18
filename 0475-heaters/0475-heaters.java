994568d893eb76541382841f7b2daa65b43c680b
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2023-02-18 14:51:49 +0530

    Time: 21 ms (44.18%), Space: 43.7 MB (97.39%) - LeetHub

---
+++ 0475-heaters/0475-heaters.java
@@ -0,0 +1,53 @@
+class Solution {
+    public static class Pair{
+        int js = -1;
+        int jg = -1;
+        Pair(){
+            
+        }
+        Pair(int js , int jg){
+            this.js = js;
+            this.jg = jg ;
+        }
+    }
+    public Pair binary_search(int key , int [] arr ){
+        Pair p = new Pair() ;
+        int start =0 ;
+        int end = arr.length  - 1;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            if(arr[mid] == key){
+                p.js = arr[mid] ;
+                p.jg = arr[mid] ;
+                break;
+            }
+            else if(arr[mid] < key){
+                p.js = arr[mid] ;
+                start = mid + 1;
+            }
+            else if(arr[mid] > key){
+                p.jg = arr[mid] ;
+                end = mid - 1;
+            }
+        }
+        return p; 
+        
+    }
+    
+    public int findRadius(int[] houses, int[] heaters) {
+        Arrays.sort(heaters) ;
+        int n = houses.length ;
+        int ans = 0;
+        for(int i =0 ; i < n ; i ++){
+            //for every house find the mini distance in left or right ;
+            Pair p = binary_search(houses[i] , heaters) ;
+            int d1 = (p.js == -1) ? Integer.MAX_VALUE : houses[i] -  p.js;
+            int d2 = (p.jg == -1) ? Integer.MAX_VALUE : p.jg - houses[i] ;
+            int res = Math.min(d1, d2);
+            if(res > ans){
+                ans = res ;
+            }
+        }
+        return ans ;
+    }
+}
