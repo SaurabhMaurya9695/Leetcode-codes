89aa5e7dcc64b9b893e8913fcd290aa3a7c76b28
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2023-02-24 22:01:41 +0530

    Added solution - LeetHub

--- Row with max 1s - GFG/row-with-max-1s.cpp
+++ Row with max 1s - GFG/row-with-max-1s.cpp
@@ -1,46 +1,52 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
 
- // } Driver Code Ends
+// } Driver Code Ends
 //User function template for C++
 class Solution{
 public:
-
-    int transitionPoint(vector<int> arr , int left, int right) {
+    
+    int solve(vector<int> arr , int start , int end){
+        // first transistion point idx 
         int ans = -1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (arr[mid] == 0) {
-                left = mid + 1;
-            } else {
-                ans = mid;
-                right = mid - 1;
+        while(start <= end){
+            int mid = (start + end) / 2;
+            if(arr[mid] == 1){
+                ans = mid ;
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
             }
         }
-
-        return ans;
+        return ans  ;
+        
     }
     
-	int rowWithMax1s(vector<vector<int> > arr, int n, int m)
-	{
-	    int right = m - 1;
-        int ans = -1, noOfOnes = 0;
-        for (int i = 0; i < n; i++) {
-            int transitionPt = transitionPoint(arr[i], 0, right);
-            if (transitionPt != -1 && m - transitionPt > noOfOnes) {
-                ans = i;
-                noOfOnes = m - transitionPt;
-                right = transitionPt - 1;
-            }
-        }
-        return ans;
+	int rowWithMax1s(vector<vector<int> > arr, int n, int m) {
+	    // code here
+	    int res = -1 ;
+	    int currone = 0 ;
+	    for(int i = 0 ; i < n ; i ++){
+	       int ftranidx = solve(arr[i] , 0, m - 1);
+	       if(ftranidx != -1){
+	           int one = m - ftranidx ;
+	           if(one > currone){
+	               currone = one ;
+	               res = i ;
+	           }
+	       }
+	       else{
+	           continue;
+	       }
+	    }
+	    return res ;
 	}
 
 };
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 int main() {
     int t;
     cin >> t;
@@ -59,4 +65,5 @@ int main() {
     }
     return 0;
 }
-  // } Driver Code Ends
+
+// } Driver Code Ends
