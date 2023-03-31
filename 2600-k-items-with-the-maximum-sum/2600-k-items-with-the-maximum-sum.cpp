9132355c89dc4147f52facdc663914b841d63d4b
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2023-03-31 23:11:55 +0530

    Time: 4 ms (57.78%), Space: 6 MB (66.59%) - LeetHub

---
+++ 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        return min(k, numOnes) -  max(0, k - numZeros - numOnes);
+        
+    }
+};
