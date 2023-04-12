0ac462bf5990ddf61d0aab74db2a1944bab5747e
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2023-04-12 12:10:11 +0530

    Time: 231 ms (43.37%), Space: 76.1 MB (37.77%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.cpp
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -5,6 +5,7 @@ public:
         unordered_map<int, vector<int>> indicesOfValue;
         for (int i = 0; i < n; i++)
             indicesOfValue[arr[i]].push_back(i);  // creating a map from where we can go
+        // for tracing the nodes ;
         vector<bool> visited(n); 
         visited[0] = true; // start from 0 so mark as true;
         queue<int> q; 
@@ -13,24 +14,24 @@ public:
         while (!q.empty()) {
             for (int size = q.size(); size > 0; --size) 
             {
-                int i = q.front(); 
+                int front = q.front(); 
                 q.pop();
-                if (i == n - 1) return step; // Reached to last index
-                vector<int>& next = indicesOfValue[arr[i]];
-                next.push_back(i - 1);
-                next.push_back(i + 1);
+                if (front == n - 1) return step; // Reached to last index
+                vector<int>& next = indicesOfValue[arr[front]];
+                next.push_back(front - 1);
+                next.push_back(front + 1);
                 for (int j : next) 
                 {
-                    if (j >= 0 && j < n && !visited[j]) 
+                    if (j >= 0 && j < n && !visited[j])  // we have to add back on queue
                     {
-                        visited[j] = true;
-                        q.push(j);
+                        visited[j] = true; // marks as true;
+                        q.push(j); // and push in queue
                     }
                 }
-                indicesOfValue[arr[i]].clear(); // avoid later lookup indicesOfValue arr[i]
+                indicesOfValue[arr[front]].clear(); // avoid later lookup indicesOfValue arr[i]
             }
             step++;
         }
-        return 0;
+        return step;
     }
 };
