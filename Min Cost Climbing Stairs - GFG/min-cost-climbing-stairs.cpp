0efd0665c48fae6171d2f380be14d78748133a06
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2023-04-12 16:56:52 +0530

    Added solution - LeetHub

--- Min Cost Climbing Stairs - GFG/min-cost-climbing-stairs.cpp
+++ Min Cost Climbing Stairs - GFG/min-cost-climbing-stairs.cpp
@@ -10,29 +10,58 @@ using namespace std;
 
 class Solution {
   public:
-    int solve(int sr , int dc , vector<int> cost , vector<int>&dp)
-    {
-        if(sr == dc) return cost[sr];
-        if(sr > dc) return  0;
-        // lookup table ;
-        if(dp[sr] != -1)
-        {
-            return dp[sr];
-        }
-        int left = solve(sr + 1 ,dc , cost , dp) + cost[sr];
-        int right = solve(sr + 2 ,dc , cost , dp) + cost[sr];
-        return dp[sr] = min(left , right) ;
-    }
+    // int solve(int sr , int dc , vector<int> cost , vector<int>&dp)
+    // {
+    //     if(sr == dc) return cost[sr];
+    //     if(sr > dc) return  0;
+    //     // lookup table ;
+    //     if(dp[sr] != -1)
+    //     {
+    //         return dp[sr];
+    //     }
+    //     int left = solve(sr + 1 ,dc , cost , dp) + cost[sr];
+    //     int right = solve(sr + 2 ,dc , cost , dp) + cost[sr];
+    //     return dp[sr] = min(left , right) ;
+    // }
   
     int minCostClimbingStairs(vector<int>&cost ,int N) {
-       int n = cost.size() ;
+      int n = cost.size() ;
         
-        vector<int> dp(n +  1 , -1);
-        int zidx = solve(0 , n - 1 , cost , dp) ;
-        int fidx = solve( 1 , n - 1 , cost , dp);
-          return min(zidx , fidx);
+    //     vector<int> dp(n +  1 , -1);
+    //     int zidx = solve(0 , n - 1 , cost , dp) ;
+    //     int fidx = solve( 1 , n - 1 , cost , dp);
+    //       return min(zidx , fidx);
+    
+                //tabulation
+        //larger problem to reach at nth idx ;
+        // smaller probelm 
+        vector<int> dp (n + 1);
+        dp[n - 1] = cost[n -1] ;
+        for(int i = n - 2 ; i>= 0 ; i--){
+            dp[i] = min(dp[i+1] , dp[i+2]) + cost[i] ;
+        }
+        
+        return min(dp[0] , dp[1]) ;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //{ Driver Code Starts.
 
