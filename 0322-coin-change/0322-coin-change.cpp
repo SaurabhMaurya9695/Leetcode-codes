243779bb24ac2f28b2797db39ac61a75e3672705
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2023-04-21 11:59:40 +0530

    Time: 93 ms (70.52%), Space: 14.6 MB (64.12%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    int help(vector<int>& coins, int amount, int n,vector<vector<int>>&h)
-    {
-        if(amount == 0) return 0;
-        if(n < 0 || amount < 0) return INT_MAX-1;
-        if(h[n][amount]!= -1) return h[n][amount];
-        int one = help(coins,amount,n-1,h);
-        int two = 1+help(coins,amount-coins[n],n,h);
-        h[n][amount] = min(one,two);
-        return h[n][amount];
-    }
-    
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>>h(coins.size()+1,vector<int>(amount+1,-1));
-        int ans = help(coins,amount,coins.size()-1,h);
-        return (ans < INT_MAX-1)?ans:-1;
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i >= coin && dp[i - coin] != INT_MAX) {
+                    dp[i] = min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount] != INT_MAX ? dp[amount] : -1;
     }
 };
