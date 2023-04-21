a4d543d5c68ebd24bc0fb79dd2e64cdfe95ac631
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2023-04-21 11:13:36 +0530

    Time: 166 ms (17.04%), Space: 50.2 MB (9.95%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
-    #define ll long long 
-    int memo(int amount, vector<int>& coins, int dp[]) {
-        if (amount == 0)
-            return 0;
-        if (dp[amount] != -1)
-            return dp[amount];
-
-        int minCoins = INT_MAX;
-        for (int i = 0; i < coins.size(); i++) {
-            if (amount - coins[i] >= 0) {
-                minCoins = min(minCoins, memo(amount - coins[i], coins, dp));
-            }
-        }
-
-        if (minCoins < INT_MAX)
-            minCoins += 1;
-        return dp[amount] = minCoins;
+    int help(vector<int>& coins, int amount, int n,vector<vector<int>>&h)
+    {
+        if(amount == 0) return 0;
+        if(n < 0 || amount < 0) return INT_MAX-1;
+        if(h[n][amount]!= -1) return h[n][amount];
+        int one = help(coins,amount,n-1,h);
+        int two = 1+help(coins,amount-coins[n],n,h);
+        h[n][amount] = min(one,two);
+        return h[n][amount];
     }
-
+    
     int coinChange(vector<int>& coins, int amount) {
-        ll n = coins.size() ;
-        int dp [amount + 1];
-        memset(dp , -1 ,sizeof(dp)) ;
-        int ans = memo(amount, coins, dp);
-        return (ans == INT_MAX) ? -1 : ans;
+        vector<vector<int>>h(coins.size()+1,vector<int>(amount+1,-1));
+        int ans = help(coins,amount,coins.size()-1,h);
+        return (ans < INT_MAX-1)?ans:-1;
     }
 };
