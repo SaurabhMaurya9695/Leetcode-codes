9722e4010c9cf685b663ab4ad32f0c2db9e8b2e3
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2023-04-21 13:38:24 +0530

    Time: 2 ms (60.83%), Space: 6.7 MB (72.45%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
-    
-    // this is permutation varience
-    int solve(vector<int> nums , int target  , vector<int> &dp){
+    int combinationSum4(vector<int>& nums, int amount) {
+        vector<unsigned int> dp(amount+1, 0);
+        dp[0] = 1; // Ways to reach dest when src = dest is 1. (Empty String)
         
-        if(target < 0 ) return 0;
-        if(target == 0){
-            return 1;
-        }
-        
-        if(dp[target] != -1){
-            return dp[target] ;
-        }
-        int ans = 0;
-        for(int i = 0; i < nums.size() ; i ++){
-            if(target - nums[i] >= 0){
-                ans += solve(nums , target - nums[i] , dp);
+        for(int i=1; i<= amount; i++){
+            for(int coin: nums){
+                if(i >= coin){
+                    dp[i] += dp[i - coin];
+                }
             }
         }
-        return dp[target] = ans ;
-    }
-    
-    int combinationSum4(vector<int>& nums, int target) {
-        vector<int > dp(1001 , -1) ;
-        return solve(nums , target , dp) ;
+        return dp[amount];
     }
 };
