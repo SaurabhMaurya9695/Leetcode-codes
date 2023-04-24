eec968f8ed6ed9897ec405ad49ae7324a8161c38
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2023-04-24 13:54:00 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -13,36 +13,22 @@ public:
     
     int knapSack(int N, int W, int val[], int wt[])
     {
-        vector<vector<int>> dp(N + 1 , vector<int>(W + 1 , 0)) ;
-        // we are doing tabulation of 1st approach , in which we are choosing one item
-        // many number of times unit our capacity become zero;
-        // we are choosing one item on one level so our dp is filling (your choice) row
-        // by row first for first item , then second item
-        //   0 1 2 3 4 5 6 7 8
-        // 0 0 0 0 0 0 0 0 0 0
-//(1,1) // 1 0
-//(3,4) // 2 0
-//(4,5) // 3 0 
-        // .
-        // .
-        
-        // if capacity is zero then all zero ;
-        // if we is zero then all are zero ;
-        // now filling from the first col = 1, row = 1;
+        vector<int> dp(W + 1);
         for(int item =1 ; item <= N ; item ++){
             for(int cap =1; cap <= W ; cap ++){
                 int yes = (cap >= wt[item - 1] ) 
-                        ? dp[item][cap - wt[item - 1]] + val[item - 1] 
+                        ? dp[cap - wt[item - 1]] + val[item - 1] 
                         : -1 ;
-                int no = dp[item - 1 ][cap];
+                int no = dp[cap];
                 
-                dp[item][cap] = max(yes , no);
+                dp[cap] = max(yes , no);
             }
         }
         
-        return dp[N][W  ];
+        return dp[W  ];
     }
 };
+
 
 //{ Driver Code Starts.
 
