2b86b8f598ff27e34772f9eb4d626d2e3c70c1ba
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2023-04-26 18:38:35 +0530

    Time: 3 ms (85.92%), Space: 11.3 MB (81.68%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -4,19 +4,20 @@ public:
         if (prices.size() <= 1)
             return 0;
 
-        int buy[prices.size()]  ;
-        int sell[prices.size()]  ;
-
-        buy[0] = -prices[0];
-        sell[0] = 0;
-        buy[1] = max(-prices[0], -prices[1]); // Either buy 0th stock or 1st stock
-        sell[1] = max(0, prices[1] - prices[0]); // Either do nothing or Buy 0 Sell 1
+        int buy0 = -prices[0];
+        int sell0 = 0;
+        int buy1 = max(-prices[0], -prices[1]); // Either buy 0th stock or 1st stock
+        int sell1 = max(0, prices[1] - prices[0]); // Either do nothing or Buy 0 Sell 1
 
         for (int i = 2; i < prices.size(); i++) {
-            buy[i] = max(buy[i - 1], sell[i - 2] - prices[i]);
-            sell[i] = max(sell[i - 1], buy[i - 1] + prices[i]);
+            int newbuy = max(buy1, sell0 - prices[i]);
+            int newsell = max(sell1, buy1 + prices[i]);
+            buy0 = buy1;
+            sell0 = sell1;
+            buy1 = newbuy;
+            sell1 = newsell;
         }
 
-        return sell[prices.size() - 1];
+        return sell1;
     }
 };
