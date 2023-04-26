15f44e8b4d543729ec6df4753dd83cf15db66622
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2023-04-26 18:36:48 +0530

    Time: 0 ms (100.00%), Space: 11.1 MB (99.18%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
-    int dp[5001][2];
-    int solve(vector<int>& cost , int buy , int idx){
-        
-        
-        if(idx >= cost.size()){
-            return 0 ; // if we reached at last idx then our profit will be zero 
-        }
-        
-        if(dp[idx][buy] != -1){
-            return dp[idx][buy];
-        }
-        //every idx has two possiblility's ti buy or sell ;
-        int ans = 0 ;
-        if(buy == 1){ // we have given idx to buy
-            int take =  -cost[idx] + solve(cost , 0 , idx + 1) ; // selling on this index 
-            int notTake = 0 + solve(cost , 1 , idx + 1) ; // we are buying on this day ;
-            ans = max(take , notTake) ;
-        }
-        else if(buy == 0) // buy == 0 means previous we buy somewhere we have to sell now ;
-        {
-            int take2 =  cost[idx] + solve(cost , 1 , idx + 2) ; // on selling we adding our price and from next idx we are buying again ; 
-            int notTake2 = 0 + solve(cost , 0 , idx + 1) ; // if we didn't sell on this day , so add profit as 0 and selling on next day ;
-            ans = max(take2 , notTake2) ;
-        }
-        
-        return dp[idx][buy] = ans ;
-    }
     int maxProfit(vector<int>& prices) {
-        memset(dp ,-1 , sizeof(dp)) ;
-        return solve(prices , 1 , 0) ; 
+        if (prices.size() <= 1)
+            return 0;
+
+        int buy[prices.size()]  ;
+        int sell[prices.size()]  ;
+
+        buy[0] = -prices[0];
+        sell[0] = 0;
+        buy[1] = max(-prices[0], -prices[1]); // Either buy 0th stock or 1st stock
+        sell[1] = max(0, prices[1] - prices[0]); // Either do nothing or Buy 0 Sell 1
+
+        for (int i = 2; i < prices.size(); i++) {
+            buy[i] = max(buy[i - 1], sell[i - 2] - prices[i]);
+            sell[i] = max(sell[i - 1], buy[i - 1] + prices[i]);
+        }
+
+        return sell[prices.size() - 1];
     }
 };
