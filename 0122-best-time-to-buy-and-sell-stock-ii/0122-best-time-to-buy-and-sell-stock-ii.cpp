7b9e7c6eec74fb704b6655849747d5beec7149df
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2023-04-26 11:10:54 +0530

    Time: 10 ms (45.07%), Space: 13.2 MB (33.39%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
-    
-    int dp[30001][2];
-    int solve(vector<int>& cost , int buy , int idx){
-        
-        
-        if(idx == cost.size()){
-            return 0 ; // if we reached at last idx then our profit will be zero 
-        }
-        
-        if(dp[idx][buy] != -1){
-            return dp[idx][buy];
-        }
-        //every idx has two possiblility's ti buy or sell ;
-        int ans = 0 ;
-        if(buy == 1){ // we have given idx to buy
-            int take =  -cost[idx] + solve(cost , 0 , idx + 1) ; // selling on this index 
-            int notTake = 0 + solve(cost , 1 , idx + 1) ; // we are buying on this day ;
-            ans = max(take , notTake) ;
-        }
-        else if(buy == 0) // buy == 0 means previous we buy somewhere we have to sell now ;
-        {
-            int take2 =  cost[idx] + solve(cost , 1 , idx + 1) ; // on selling we adding our price and from next idx we are buying again ; 
-            int notTake2 = 0 + solve(cost , 0 , idx + 1) ; // if we didn't sell on this day , so add profit as 0 and selling on next day ;
-            ans = max(take2 , notTake2) ;
-        }
-        
-        return dp[idx][buy] = ans ;
-    }
     int maxProfit(vector<int>& prices) {
-        memset(dp ,-1 , sizeof(dp)) ;
-        return solve(prices , 1 , 0) ; // we are making first idx as buying day 
+        int n = prices.size();
+        int buy[ n + 1];
+        int sell[n + 1] ;
+        if(n == 1){
+            return 0 ;
+        }
+
+        buy[0] = -prices[0];
+        sell[0] = 0;
+        int ans [n + 1];
+ 
+        for (int i = 1; i < prices.size(); i++) {
+
+            // Treat today as Buying Day
+            buy[i] = max(buy[i - 1], sell[i - 1] - prices[i]);
+
+            // Treat today as Selling Day
+            sell[i] = max(sell[i - 1], buy[i - 1] + prices[i]);
+            ans[i] = max(buy[i] , sell[i]) ;
+        }
+
+        return ans[n- 1];
     }
 };
