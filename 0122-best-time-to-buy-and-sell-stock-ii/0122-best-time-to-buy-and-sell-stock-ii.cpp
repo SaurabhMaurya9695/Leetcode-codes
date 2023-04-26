906a200aa189749a6c3bc5a001f1fca362196bbe
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2023-04-26 10:01:07 +0530

    Time: 3 ms (95.54%), Space: 15.3 MB (18.45%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,17 +1,35 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int buyingDay = prices[0] ;
-        int ans = 0;
-        for(int i =  0; i < prices.size() ; i ++){
-            if(buyingDay < prices[i]){
-                ans += abs(prices[i] - buyingDay);
-                
-            }
-            buyingDay = prices[i];
-            
+    
+    int dp[30001][2];
+    int solve(vector<int>& cost , int buy , int idx){
+        
+        
+        if(idx == cost.size()){
+            return 0 ; // if we reached at last idx then our profit will be zero 
         }
         
-        return ans ;
+        if(dp[idx][buy] != -1){
+            return dp[idx][buy];
+        }
+        //every idx has two possiblility's ti buy or sell ;
+        int ans = 0 ;
+        if(buy == 1){ // we have given idx to buy
+            int take =  -cost[idx] + solve(cost , 0 , idx + 1) ; // selling on this index 
+            int notTake = 0 + solve(cost , 1 , idx + 1) ; // we are buying on this day ;
+            ans = max(take , notTake) ;
+        }
+        else if(buy == 0) // buy == 0 means previous we buy somewhere we have to sell now ;
+        {
+            int take2 =  cost[idx] + solve(cost , 1 , idx + 1) ; // on selling we adding our price and from next idx we are buying again ; 
+            int notTake2 = 0 + solve(cost , 0 , idx + 1) ; // if we didn't sell on this day , so add profit as 0 and selling on next day ;
+            ans = max(take2 , notTake2) ;
+        }
+        
+        return dp[idx][buy] = ans ;
+    }
+    int maxProfit(vector<int>& prices) {
+        memset(dp ,-1 , sizeof(dp)) ;
+        return solve(prices , 1 , 0) ; // we are making first idx as buying day 
     }
 };
