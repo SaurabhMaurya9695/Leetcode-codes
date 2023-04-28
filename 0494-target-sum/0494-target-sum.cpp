caec9917c951862d3b0dddc6274a42f20e739bdb
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2023-04-28 14:10:22 +0530

    Time: 19 ms (60.34%), Space: 39.9 MB (17.16%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
-    int dp[21][2005] ;
-    int solve(int idx , vector<int>& arr , int target , int n )
-	{
-	    if (target < 0)
-            return 0;
-        if (idx == n) {
-            if (target == 0)
-                return 1;
-            return 0;
-        }
-        if (dp[idx][target] != -1)
-            return dp[idx][target] ;
-
-        int no = solve(idx + 1, arr , target, n );
-        int yes = solve(idx + 1, arr , target - arr[idx], n );
-        return dp[idx][target ] = (no + yes) ;
-	}
-    
+    vector<vector<int>> dp;
+    int helper(int i,vector<int> &nums,int target,int n){
+        if(i==n and target==0) return 1;
+        if(i==n) return 0;
+        if(dp[i][target+1000]!=-1) return dp[i][target+1000];
+        int a=0;
+        int left =helper(i+1,nums,target-nums[i],n);
+        int right = helper(i+1,nums,target+nums[i],n);
+        return dp[i][target+1000]= left + right;
+    }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int totalsum = 0 ;
-        for(int x : nums)  totalsum += x;
-        //we are assuming that we are dividing a subset into the two set where s1-s2  = target
-        //positive element in one set and -ive element in another set ;
-        
-        if(target  > totalsum) return 0;
-        if((totalsum - target ) % 2 == 1)
-        {
-            return 0;
-        }
-        int targetx = (totalsum - target ) / 2 ;
-        memset(dp ,-1, sizeof(dp)) ;
-        return solve(0 , nums  , targetx  ,  nums.size() ) ;
+        int n=nums.size(),s=0;
+        dp.resize(n+1,vector<int>(target+2001,-1));
+        return helper(0,nums,target,n);
     }
 };
