b4e9b8986bdd7abc2403395651450298fc7f61bf
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2023-05-01 12:25:16 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (56.27%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        // for 2 * 3 -> paths are ;
-        // r r d , d r r , r d r  
-        // now you observe for each every m no of coloum -> you have to go (m - 1) times right ;
-        // and for every row -> you have to go (n - 1) dowm ;
-        // (m - 1) + (n - 1) = > (m + n - 2) ;
-        // you need to fill in 3 boxex out of 2 right or 1 left ;
-        // (m + n - 2) C (n - 1) or (m + n - 2) C (m - 1) 
-        int N = m + n - 2;
-        int r = m - 1 ;
-        double res = 1 ;
-        // nCr = n * (n - 1) * (n - 2) .... / 1 * 2 * 3 ... r
-        for(int i = 1; i <= r; i ++){
-            res = res *  (N - r + i) / i ;
+        vector<vector<int>>dp(m , vector<int>(n)) ;
+        dp[m-1][n-1] = 1;
+        for(int r = m-1; r >= 0; r--){
+            for(int c = n-1; c >= 0; c--){
+                if(r+1 < m){
+                    dp[r][c] += dp[r+1][c];
+                }
+                else{
+                    dp[r][c] += 0;
+                }
+                if(c+1 < n){
+                    dp[r][c] += dp[r][c+1];
+                }
+                else{
+                    dp[r][c] += 0;
+                }
+            }
         }
-
-        return (int)res ;
+        return dp[0][0];
     }
 };
