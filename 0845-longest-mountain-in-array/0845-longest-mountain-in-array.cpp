6933a8edb3fa6a50ab508315ebd7213d9c99c774
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2023-05-05 18:03:51 +0530

    Time: 27 ms (21.85%), Space: 18.5 MB (92.22%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    int longestMountain(vector<int>& nums) {
-        int n = nums.size() ;
-        // Longest increasing subarray ending at index i
-        vector<int> prefix(n , 1);
-        for (int i = 1; i < n; i++) {
-            if (nums[i - 1] < nums[i]) {
-                prefix[i] = prefix[i - 1] + 1;
+    int longestMountain(vector<int>& arr)
+    {
+        int ans=0;
+        int n=arr.size();
+        for(int i=1;i<arr.size()-1;i++)
+        {
+            //treating every element as a peak;
+            int start=i;
+            int end=i;
+            
+            //before exploring first check whether exist in left or right 
+            //then go in left deep and right deep ;
+            if(arr[i]>arr[i-1]&&arr[i]>arr[i+1])
+            {
+                // go till start exist
+                while(start>0&&arr[start]>arr[start-1]){
+                    start--;
+                }
+                
+                // go till end exist
+                while(end<n-1 &&arr[end]>arr[end+1]){
+                    end++;
+                }
+                
+                ans=max(end-start+1,ans); // our length our mountain is end-start+1
+                end=end+1; // already calculated for start ..check kro end now 
             }
         }
-
-        vector<int> suffix(n , 1);
-        // Longest decreasing subarray starting at index i
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] > nums[i + 1]) {
-                suffix[i] = suffix[i + 1] + 1;
-            }
-        }
-
-        
-        // Longest Bitonic Subarray = LIS + LDS - 1
-        int ans = 0;
-        for (int i = 0; i < n ; i++) {
-            if (prefix[i] > 1 && suffix[i] > 1) {
-                ans = max(ans , prefix[i] + suffix[i] - 1);
-            }
-        }
-
         return ans;
     }
 };
