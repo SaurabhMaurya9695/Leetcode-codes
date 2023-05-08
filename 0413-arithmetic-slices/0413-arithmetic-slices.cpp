63d3154d037f5012386e19c1d658eeba3fc15e16
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2023-05-08 11:49:30 +0530

    Time: 3 ms (46.45%), Space: 7.3 MB (52.75%) - LeetHub

---
+++ 0413-arithmetic-slices/0413-arithmetic-slices.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& arr)
+    {
+        int n = arr.size() ;
+        if(n < 3) return 0;
+        int diff = arr[1] - arr[0] ;
+        int cnt = 0;
+        int idx = 0; // to storing the subarray;
+        // starting from a index 1 ;
+        for(int i = 1 ; i< n -1 ; i++)
+        {
+            int d = arr[i+1] - arr[i];
+            if(d == diff)
+            {
+                idx++; // we get the subarray 
+            }
+            else{
+                diff = d;
+                idx = 0;
+            }
+            cnt += idx ; // number of subarray ending at  i is this idx ;
+        }
+        return cnt ;
+        
+    }
+};
