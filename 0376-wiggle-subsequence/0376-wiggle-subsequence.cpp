80462e63d0fd8ec7f90623dbbbdc3f9215486a60
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2023-05-09 11:01:55 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (42.04%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
@@ -1,35 +1,63 @@
 class Solution {
 public:
+    
+    int dp[1005][2];
+    
+    int helper(vector<int>& nums, int i, int n, bool flag)
+    {
+        // if one element is present
+        if(i == n - 1){
+            return 1;
+        }
+        
+        // if already calculated
+        if(dp[i][flag] != -1)
+            return dp[i][flag];
+        
+        int ans = 0;
+        
+        if(flag)  // we need positive diff.
+        {
+            if(nums[i + 1] > nums[i])  // include
+            {
+                ans = max(ans, 1 + helper(nums, i + 1, n, false));
+            }
+            else  // exclude
+            {
+                ans = max(ans, helper(nums, i + 1, n, true));
+            }
+        }
+        else   // we need negative diff
+        {
+            if(nums[i + 1] < nums[i])   // include
+            {
+                ans = max(ans, 1 + helper(nums, i + 1, n, true));
+            }
+            else  // exclude
+            {
+                ans = max(ans, helper(nums, i + 1, n, false));
+            }
+        }
+        
+        // store the ans in dp   
+        return dp[i][flag] = ans;
+    }
+    
     int wiggleMaxLength(vector<int>& nums) {
-        // if the last diff is positive then we can't add the positive again
-        // we need to add -ive this time , and if last diff is 
-        // -ive then we have to add +ive .
         
-        int n = nums.size() ;
-        if(n < 2) {
-            return n ;
-        }
+        int n = nums.size();
         
-        int prev = nums[1] - nums[0] ; // if it is +ive then search for -ive or vice versa
-        int cnt = (prev != 0 ? 2 : 1) ;// minimum 2 length subsequence should be present ;
-        for(int i = 2 ; i < n ; i++){
-            int diff = nums[i] - nums[i-1] ;
-            if(diff > 0 && prev <= 0){
-                cnt ++ ;
-                prev = diff;
-            }
-            
-            if(diff < 0 && prev >= 0){
-                cnt ++ ;
-                prev = diff;
-            }
-            
-            // if both are same , prev and diff then just skip that element
-            if((prev > 0 && diff > 0 ) or (prev < 0 && diff < 0)){
-                continue;
-            }
-            
-        }
-        return cnt  ;
+        int ans = INT_MIN;
+        
+        memset(dp, -1, sizeof(dp));
+        // by taking positive as first diff.
+        
+        ans = max(ans, helper(nums, 0, n, true));
+        
+        memset(dp, -1, sizeof(dp));
+        // by taking negative as first diff.
+        
+        ans = max(ans, helper(nums, 0, n, false));
+        return ans;
     }
 };
