773637302be380fdb068889a024c6d20e9635504
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2023-05-25 11:45:08 +0530

    Time: 7 ms (41.54%), Space: 10.7 MB (5.16%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
-    
+    int dp[21][21] ;
     int solve(vector<int> nums , int left , int right ){
         if(left > right) return 0 ;
         if(left == right) return nums[left] ;
         
+        if(dp[left][right] != -1){
+            return dp[left][right];
+        }
         //draw tree and observe calls 
         // these call made by player 1
         int c1 = solve(nums , left + 2 , right) ;
@@ -13,12 +16,12 @@ public:
         
         //player 1 wants the max sum but for player 2 min would be added ;
         
-         return max(nums[left] + min(c1, c2) , nums[right] + min(c2 , c3)) ;        
+         return dp[left][right] = max(nums[left] + min(c1, c2) , nums[right] + min(c2 , c3)) ;        
     }
     bool PredictTheWinner(vector<int>& nums) {
         // player 1 choose max for himself and player 2 min for player 1 ;
         // we. can ignore the call for player 2 but it will pass the min value to player 1
-        
+        memset(dp ,-1 , sizeof(dp)) ;
         int player1_score =  solve(nums , 0 , nums.size() - 1);
         
         int sum = 0 ;
