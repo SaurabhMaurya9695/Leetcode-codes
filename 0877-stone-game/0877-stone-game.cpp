3c8497549e6a55503151ceb02587a9d111f6f1f9
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2023-05-26 21:22:34 +0530

    Time: 23 ms (33.88%), Space: 8.7 MB (39.88%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,47 +1,21 @@
-// if player 1 choose odd idx then he force to the opprosit person to choose even index
-// and vice versa
-// this solution work for only even idx;
-
-
-// so ans is always true
 class Solution {
 public:
-    int dp[501][501][2];
-    bool solve(int turn , vector<int>& piles ,int ali , int bob ,int start , int end)
-    {
-        if(start == end){
-            if(turn == 0) 
-            {
-                ali += piles[start];
-            }
-            else {
-                bob += piles[end];
-            }
-            return ali > bob;
+    
+    int dp[501][501] ;
+    int solve(int i , int j , vector<int> &stone){
+        if(i > j) return 0 ;
+        
+        if(dp[i][j] != -1){
+            return dp[i][j] ;
         }
-
-        if(dp[start][end][turn] != -1) 
-        {
-            return dp[start][end][turn];
-        }
-
-        if(turn == 0) // alice turn 
-        {
-            // he can choose from start or from end ;
-            dp[start][end][turn] =  solve(1 , piles , ali + piles[start] , bob , start + 1 ,end) || 
-                              solve(1 , piles , ali + piles[end] , bob , start , end-1);
-        }
-        else {
-            // bob turn 
-            dp[start][end][turn]  =  solve(0 , piles , ali , bob + piles[start], start+1 ,end) ||  
-                              solve(0 , piles , ali , bob + piles[end] , start , end-1);
-        }
-
-        return dp[start][end][turn];
+        int taken_from_start = stone[i] - solve(i + 1, j , stone);
+        int taken_from_end   = stone[j] - solve(i , j - 1 , stone);
+        
+        return dp[i][j] = max(taken_from_start , taken_from_end) ;
     }
-
-    bool stoneGame(vector<int>& piles) { 
-        memset(dp ,-1 , sizeof(dp)) ;
-        return solve(0,piles,0,0,0,piles.size()-1);
+    
+    bool stoneGame(vector<int>& piles) {
+        memset(dp ,-1, sizeof(dp));
+        return solve(0 , piles.size() - 1 , piles) > 0;
     }
 };
