59802e307326588764581230a4df4daef8928be1
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2023-06-03 11:11:16 +0530

    Time: 439 ms (53.05%), Space: 19.7 MB (33.29%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -13,30 +13,37 @@ public:
         return true;
     }
     
-    int dp[2001] ;
-    int solve(int i , string &s){
-        
-        if(i == s.size()){
-            return  0 ;
-        }
-        
-        if(dp[i] != -1){
-            return dp[i] ;
-        }
-        int minCost = INT_MAX ;
-        string temp = "";
-        for(int j = i ; j < s.size() ; j ++){
-            temp += s[j] ;
-            if(isPalindrome(temp) == true){
-                int cost = 1 + solve(j + 1, s);
-                minCost = min(minCost,  cost) ;
-            }
-        }
-        
-        return dp[i] = minCost ;
-    }
     int minCut(string s) {
-        memset(dp ,-1 , sizeof(dp)) ;
-        return solve(0 , s) - 1; 
+        int n = s.size() ;
+        vector<int> dp (n + 1,  0) ;
+        
+        // base case 
+        dp[n] = 0;
+        for(int i = n- 1 ; i>= 0 ; i--){
+            int minCost = INT_MAX ;
+            string temp = "";
+            for(int j = i ; j < s.size() ; j ++){
+                temp += s[j] ;
+                if(isPalindrome(temp) == true){
+                    int cost = 1 + dp[j + 1] ;
+                    minCost = min(minCost,  cost) ;
+                }
+            }
+            dp[i] = minCost ;
+        }
+        
+        return dp[0] - 1 ;
+        
     }
 };
+
+
+
+
+
+
+
+
+
+
+
