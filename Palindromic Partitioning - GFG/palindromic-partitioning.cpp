94d0fd973d2cf53209638daae4be384a193c4a40
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2023-06-03 13:04:09 +0530

    Added solution - LeetHub

---
+++ Palindromic Partitioning - GFG/palindromic-partitioning.cpp
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+// Initial Template for c++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    
+    // int dp[501] ;
+    bool ispalindrome(string &s){
+        int n = s.size() ;
+        for(int i = 0 ; i< n / 2;  i ++){
+            if(s[i] != s[ n - i - 1]){
+                return false;
+            }
+        }
+        return true;
+    }
+    // int solve(int i , int n , string s){
+        
+    //     if(i == n){ // we make here one cut so we need to decrease 1
+    //          return 0;
+    //     }
+        
+    //     if(dp[i] != -1){
+    //         return dp[i];
+    //     }
+    //     // make a possible cuts ;
+    //     string temp = "";
+    //     int ans = INT_MAX ;
+    //     for(int j = i ; j < n ; j ++){
+    //         temp += s[j] ;
+    //         if(ispalindrome(temp)){
+    //             int cuts = 1 + solve(j + 1, n , s) ;
+    //             ans = min(ans , cuts) ;
+    //         }
+            
+            
+    //     }
+        
+    //     return dp[i] = ans ;
+    // }
+    
+    int palindromicPartition(string s)
+    {
+        // memset(dp ,-1 ,sizeof(dp)) ;
+        // return solve(0 , s.size() - 1 , s) - 1 ;
+        int n = s.size() ;
+        vector<int> dp (n + 1,  0) ;
+        
+        // base case 
+        dp[n] = 0;
+        for(int i = n- 1 ; i>= 0 ; i--){
+            int minCost = INT_MAX ;
+            string temp = "";
+            for(int j = i ; j < s.size() ; j ++){
+                temp += s[j] ;
+                if(ispalindrome(temp) == true){
+                    int cost = 1 + dp[j + 1] ;
+                    minCost = min(minCost,  cost) ;
+                }
+            }
+            dp[i] = minCost ;
+        }
+        
+        return dp[0] - 1 ;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string str;
+        cin>>str;
+        
+        Solution ob;
+        cout<<ob.palindromicPartition(str)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
