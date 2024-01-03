cb2fd5072d1c926c6af3c8bed91d489c18d4d30a
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2024-01-03 13:20:22 +0530

    Time: 197 ms (5.06%), Space: 23.4 MB (82.87%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        auto cntOnes = [&] (string s){
-            int cnt_one = 0 ;
-            for(int i = 0 ; i < s.size() ; i ++){
-                if(s[i] == '1'){
-                    cnt_one ++ ;
-                }   
+        // 3 , 2 * 1 ;
+        int last = count(bank[0].begin() , bank[0].end() ,'1');
+        int sum = 0 ;
+        for(int i = 1; i < bank.size() ; i++){
+            int x = count(bank[i].begin() , bank[i].end() ,'1');
+            if(x == 0){
+                continue;
             }
-            
-            return cnt_one ;
-        };
-        
-        string s = bank[0] ;
-        int ans = 0 ;
-        int PreZero = cntOnes(s) ;
-        // cout << PreZero
-        for(int i = 1 ; i < bank.size() ; i ++){
-            int nextRowZeros = cntOnes(bank[i]) ;
-            cout << nextRowZeros << " ";
-            if(nextRowZeros > 0){
-                ans += PreZero * nextRowZeros;
-                PreZero = nextRowZeros;
+            else{
+                sum += (x * last);
+                last = x;
             }
             
         }
-        
-        return ans ;
+        return sum;
     }
 };
