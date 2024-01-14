e622bb6fddc71602a5e0c4ab21dd40fe38295d51
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2024-01-14 12:56:42 +0530

    Time: 66 ms (90.42%), Space: 20.8 MB (89.19%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
+    
+    /*
+    word1 = "aabcccdeeeff", word2 = "abbbccddefff"
+    Both strings contain the same unique characters {a, b, c, d, e, f}.
+    Frequency charts of both strings:
+    word1: a->2, b->1, c->3, d->1, e->3, f->2
+    word2: a->1, b->3, c->2, d->2, e->1, f->3
+    Sorted order of frequency arrays: {1, 1, 2, 2, 3, 3} 
+    
+    */
     bool closeStrings(string word1, string word2) {
-        vector<int>arr1(26), arr2(26);
+        vector<int>freq(26), freqT(26);
         for(int i = 0; i < word1.size(); i++){
-            arr1[word1[i]-'a']++;
+            freq[word1[i]-'a']++;
         }
         for(int i = 0; i < word2.size(); i++){
-            arr2[word2[i]-'a']++;
+            freqT[word2[i]-'a']++;
         }
         
         for(int i = 0; i < 26; i++){
-            if(arr1[i] == 0 && arr2[i] != 0 || arr1[i] != 0 && arr2[i] == 0) return false;
+            if(freq[i] == 0 && freqT[i] != 0 || freq[i] != 0 && freqT[i] == 0) return false;
         }
-        sort(arr1.begin(), arr1.end()); 
-        sort(arr2.begin(), arr2.end());
+        sort(freqT.begin(), freqT.end()); 
+        sort(freq.begin(), freq.end());
         
         for(int i = 0; i < 26; i++){
-            if(arr1[i] != arr2[i]) return false;
+            if(freq[i] != freqT[i]) return false;
         }
         
         return true;
