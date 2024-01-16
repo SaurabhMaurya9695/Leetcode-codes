67ce55723dd497bed5fac66f098e2046a7cf7283
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2024-01-16 22:18:18 +0530

    Time: 191 ms (34.59%), Space: 97.2 MB (90.86%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -1,44 +1,29 @@
-#pragma GCC optimize("Ofast","inline","-ffast-math")
-#pragma GCC target("avx,mmx,sse2,sse3,sse4")
 class RandomizedSet {
+    unordered_set<int>s;
 public:
-    unordered_map<int, int> um; //val, index in vector
-    unordered_map<int, int>::iterator it;
-    vector<int> v;
-    
     RandomizedSet() {
-        ios::sync_with_stdio(false);
-        cin.tie(0);
-        cout.tie(0);
+       
     }
     
     bool insert(int val) {
-        if(um.count(val)) return false;
-        um[val] = v.size();
-        v.push_back(val);
-        return true;
+        if(s.find(val)!=s.end())return 0;
+        else {
+          
+           s.insert(val);
+            return 1;
+        }
     }
     
     bool remove(int val) {
-        it = um.find(val);
-        if(it == um.end()) return false;
-        v[it->second] = v.back();
-        um[v.back()] = it->second;
-        v.pop_back();
-        um.erase(it);
-        return true;
+        if(s.find(val)==s.end())return 0;
+        else {
+           
+            s.erase(val);
+            return 1;
+        }
     }
     
     int getRandom() {
-        return v[rand() % v.size()];
+        return *next(s.begin(),rand()%s.size()); 
     }
 };
-
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * RandomizedSet* obj = new RandomizedSet();
- * bool param_1 = obj->insert(val);
- * bool param_2 = obj->remove(val);
- * int param_3 = obj->getRandom();
- */
-
