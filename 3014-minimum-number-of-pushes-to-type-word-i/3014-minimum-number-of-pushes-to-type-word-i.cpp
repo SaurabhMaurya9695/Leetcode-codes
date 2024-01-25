877d3ea32d3aab4832b8479beec5cfc6d0bb2edd
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2024-01-25 21:08:23 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (24.74%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,7 +1,14 @@
 class Solution {
 public:
     int minimumPushes(string s) {
-       
+        
+        // assign every char to a number bw 2 to 9 
+        // xycdefghij
+        // {2,x} {3,y} , {4, c} ,{5,d} , {6,e} , {7,f} , {8 ,g} , {9,h} -> here we click one time only so cnt += 9
+        // then there is not option left to assign then assign next char to start
+        // {2, x,i} {3,y,j} -> here we clicked two times 2 and 3 so cnt += 4;
+        
+        
         map<int , int > mp ;
         int k = 2;
         int cnt = 0;
