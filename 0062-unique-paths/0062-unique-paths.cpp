59642b94554a11722f87c58d8913c39b95c0da77
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2024-02-12 21:26:11 +0530

    Time: 3 ms (33.42%), Space: 7.2 MB (32.64%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,34 +1,28 @@
-    //path is for 3 * 3
-    // r . . 
-    // . . .
-    // . . d
-    // we have to go from r to d 
-    // rrdd
-    // ddrr
-    // rddr
-    // rdrd
-    // drrd
-    // drdr  // so for n rows and n colums => 
-
 class Solution {
 public:
-    int uniquePaths(int m, int n) {
-        // for 2 * 3 -> paths are ;
-        // r r d , d r r , r d r  
-        // now you observe for each every m no of coloum -> you have to go (m - 1) times right ;
-        // and for every row -> you have to go (n - 1) dowm ;
-        // (m - 1) + (n - 1) = > (m + n - 2) ;
-        // you need to fill in 3 boxex out of 2 right or 1 left ;
-        // (m + n - 2) C (n - 1) or (m + n - 2) C (m - 1) 
-        int N = m + n - 2;
-        int r = m - 1 ;
-        double res = 1 ;
-        // nCr = n * (n - 1) * (n - 2) .... / 1 * 2 * 3 ... r
-        for(int i = 1; i <= r; i ++){
-            res = res *  (N - r + i) / i ;
+    
+    int dp[101][101];
+    int solve(int sr , int sc, int dr , int dc){
+        if(sr > dr || sc > dc){
+            return 0;
         }
-
-        return (int)res ;
+        
+        if(sr == dr && sc == dc){
+            return 1;
+        }
+        
+        if(dp[sr][sc] != -1){
+            return dp[sr][sc];
+        }
+        
+        int down = solve(sr + 1, sc , dr , dc);
+        int right = solve(sr , sc + 1, dr , dc);
+        
+        return dp[sr][sc] = (down + right) ;
+    }
+    
+    int uniquePaths(int m, int n) {
+        memset(dp , -1 , sizeof(dp));
+        return solve(0 ,0 ,m - 1 , n -1 );
     }
 };
-    
