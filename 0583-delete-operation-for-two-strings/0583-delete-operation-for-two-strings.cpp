c135a9f473df920dc57f13214cbebdcab4bdb991
Saurabh <53153191+SaurabhMaurya9695@users.noreply.github.com>
2024-02-20 21:34:40 +0530

    Time: 27 ms (19.53%), Space: 22.5 MB (8.57%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
-    int dp[501][501];
-    int solve(int i , int j , string s , string t){
+    
+    int solve(string s , string t){
+        int n = s.size() ;
+        int m = t.size() ;
         
-        int n = s.size();
-        int m = t.size();
+        vector<vector<int>> dp(n + 1 , vector<int>(m + 1, 0));
+        for(int i = n - 1 ; i>= 0 ; i --){
+            for(int j = m - 1; j >= 0 ; j --){
+                if(s[i] == t[j]){
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                }
+                else{
+                    dp[i][j]= 0 + max(dp[i][j + 1] , dp[i + 1][j]);
+                }
+            }
+        }
         
-        if(i == s.size() && j == t.size()){
-            return 0 ;
-        }
-        if(i == s.size() && j < t.size()){
-            return m - j ;
-        }
-        if(i < s.size() && j == t.size()){
-            return n - i;
-        }
+        return dp[0][0];
         
-        if(dp[i][j] != -1){
-            return dp[i][j] ;
-        }
-        char x = s[i];
-        char y = t[j];
-        
-        if(x != y){
-            int left_exclude = solve(i + 1 , j ,  s , t);
-            int right_exclude = solve(i , j + 1 ,  s , t);
-            return  dp[i][j] = 1 + min(left_exclude , right_exclude) ;
-        }
-        return dp[i][j] = solve(i + 1, j + 1, s,  t);
     }
-    int minDistance(string word1, string word2) {
-        memset(dp , -1 , sizeof(dp)) ;
-        return solve(0 , 0 , word1 , word2) ;
+    
+    int minDistance(string s, string t) {
+        // s = abcd & t = anc
+        // ac is common between these two string this is nothing just a LCS
+        
+        // no of element deleted from string s is n -  LCS
+        
+        // the minimum no of deletion to make
+        
+        int deletionFromS = s.size() - solve(s, t);
+        int insertionInT = t.size() - solve(s, t);
+        return deletionFromS + insertionInT ;
     }
 };
